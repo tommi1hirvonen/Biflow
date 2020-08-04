fbf69f70b4821638150f9b63b1ca5598c6adc0ea
tommi.hirvonen <tommi.1.hirvonen@gmail.com>
2020-08-04 14:02:25 +0300

    Added new overview for all schedules for all jobs. Appearance improvements.

---
+++ Pages/Schedules/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EtlManager.Data;
+using EtlManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace EtlManager.Pages.Schedules
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EtlManagerContext _context;
+
+        public IndexModel(EtlManagerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Job> Jobs { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Jobs = await _context.Jobs.Include(job => job.Schedules).ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(Guid? id)
+        {
+            if (id == null) return NotFound();
+
+            Schedule schedule = await _context.Schedules.FindAsync(id);
+
+            if (schedule == null) return NotFound();
+
+            _context.Schedules.Remove(schedule);
+            await _context.SaveChangesAsync();
+
+            Jobs = await _context.Jobs.Include(job => job.Schedules).ToListAsync();
+            return Page();
+        }
+    }
+}
