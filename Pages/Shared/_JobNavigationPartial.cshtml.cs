6875cc0d408b6878754ba54382b2e4a4fffce03e
tommi.hirvonen <tommi.1.hirvonen@gmail.com>
2020-09-20 17:01:46 +0300

    Added more partial views for JobDetails navigation

---
+++ Pages/Shared/_JobNavigationPartial.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EtlManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EtlManager.Pages.Shared
+{
+    public class _JobNavigationPartialModel : PageModel
+    {
+        public Job Job { get; set; }
+        public IList<Job> Jobs { get; set; }
+        public string RedirectPage { get; set; }
+    }
+}
