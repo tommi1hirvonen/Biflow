e9cf6cd03bb687331c0be2f3c889a6ff39f0b059
unknown <tommi.1.hirvonen@gmail.com>
2020-10-16 18:55:03 +0300

    Replaced custom timeline chart with vis.js timeline library

--- Pages/Executions/JobDetails.cshtml.cs
+++ Pages/Executions/JobDetails.cshtml.cs
@@ -32,40 +32,12 @@ namespace EtlManager.Pages.Executions
 
         public bool IsOperator { get; set; } = false;
 
-        public bool Graph = false;
-        public bool Collapsed = false;
-
+        public bool Graph = true;
         public IList<StepExecution> Executions { get; set; }
 
         public JobExecution JobExecution { get; set; }
 
-
-
-        public IList<ChartElement> ChartElements = new List<ChartElement>();
-        public IList<ChartLabel> ChartLabels = new List<ChartLabel>();
-
-        public int ChartHeight;
-        public int ChartHeightCollapsed = 500;
-
-        public int ChartPaddingTop = 25;
-
-        public int ChartPaddingBottom = 0;
-        public int ChartPaddingBottomCollapsed = 20;
-
-        public double BarHeight = 10;
-        public double BarHeightCollapsed = 10;
-
-
-        public int ChartWidth = 1200;
-        public int ChartPaddingLeft = 250;
-
-        public DateTime MinTime { get; set; }
-        public DateTime MaxTime { get; set; }
-
-        private const int MinWidth = 5;
-
-
-        public async Task OnGetAsync(Guid id, bool graph = false, bool collapsed = false)
+        public async Task OnGetAsync(Guid id, bool graph = true)
         {
             var authorized = await _authorizationService.AuthorizeAsync(User, "RequireOperator");
             if (authorized.Succeeded)
@@ -83,97 +55,11 @@ namespace EtlManager.Pages.Executions
                 .FirstOrDefaultAsync(e => e.ExecutionId == id);
 
             Graph = graph;
-            Collapsed = collapsed;
 
             if (JobExecution == null)
             {
                 JobExecution = new JobExecution { ExecutionId = id, JobName = "Waiting for execution to start..." };
                 return;
-            }
-
-            // No steps were started, no need to calculate chart properties or add chart elements.
-            if (Executions.All(e => e.StartDateTime == null))
-            {
-                return;
-            }
-
-            // Calculate chart properties
-
-            ChartHeight = Executions.Count * 35 + ChartPaddingTop;
-
-            double yInterval = (double)(ChartHeight - ChartPaddingTop - ChartPaddingBottom) / Executions.Count(e => e.StartDateTime != null);
-            double yIntervalCollapsed = (double)(ChartHeightCollapsed - ChartPaddingTop - ChartPaddingBottomCollapsed) / Executions.Count(e => e.StartDateTime != null);
-            
-            BarHeight = (double)(ChartHeight - ChartPaddingTop - ChartPaddingBottom) / Executions.Count(e => e.StartDateTime != null) / 2.0;
-            BarHeightCollapsed = (double)(ChartHeightCollapsed - ChartPaddingTop - ChartPaddingBottomCollapsed) / Executions.Count(e => e.StartDateTime != null) / 2.0;
-
-            double yLocation = 0;
-            double yLocationCollapsed = 0;
-
-            MinTime = (DateTime)Executions.Min(e => e.StartDateTime);
-
-            // If there are uncompleted steps
-            if (Executions.Any(e => e.EndDateTime == null))
-            {
-                MaxTime = Executions.Where(e => e.StartDateTime != null && e.ExecutionInSeconds != null)
-                    .Select(e => ((DateTime)e.StartDateTime).AddSeconds((double)e.ExecutionInSeconds))
-                    .Max();
-            }
-            else
-            {
-                MaxTime = (DateTime)Executions.Max(e => e.EndDateTime);
-            }
-
-            long minTicks = MinTime.Ticks;
-            long maxTicks = MaxTime.Ticks;
-
-            foreach (var step in Executions.Where(e => e.StartDateTime != null).Select(e => new KeyValuePair<Guid, string>(e.StepId, e.StepName)).Distinct())
-            {
-                ChartLabels.Add(new ChartLabel
-                {
-                    StepId = step.Key,
-                    StepName = step.Value,
-                    LabelYLocation = yLocation.ToString().Replace(',', '.'),
-                    LabelYLocationCollapsed = yLocationCollapsed.ToString().Replace(',', '.'),
-                    YLocationDouble = yLocation,
-                    YLocationCollapsedDouble = yLocationCollapsed
-                });
-                yLocation += yInterval;
-                yLocationCollapsed += yIntervalCollapsed;
-            }
-
-            foreach (var execution in Executions.Where(e => e.StartDateTime != null))
-            {
-                long startTicks = ((DateTime)execution.StartDateTime).Ticks;
-                double xLocation = (double)(startTicks - minTicks) / (maxTicks - minTicks) * (ChartWidth - ChartPaddingLeft); // normalize time range to the chart height
-
-                long endTicks;
-                if (execution.EndDateTime != null)
-                {
-                    endTicks = ((DateTime)execution.EndDateTime).Ticks;
-                }
-                else
-                {
-                    endTicks = ((DateTime)execution.StartDateTime).AddSeconds((double)execution.ExecutionInSeconds).Ticks;
-                }
-
-                double endLocation = (double)(endTicks - minTicks) / (maxTicks - minTicks) * (ChartWidth - ChartPaddingLeft); // normalize time range to the chart height
-                double width = endLocation - xLocation;
-                width = width >= MinWidth ? width : MinWidth; // minimum value for the width to prevent hidden bars with width = 0
-
-                var yLocation_ = ChartLabels.Where(label => label.StepId == execution.StepId).Select(label => label.YLocationDouble).First();
-                var yLocationCollapsed_ = ChartLabels.Where(label => label.StepId == execution.StepId).Select(label => label.YLocationCollapsedDouble).First();
-
-                ChartElements.Add(new ChartElement
-                {
-                    StepExecutionId = execution.StepExecutionId,
-                    ExecutionStatus = execution.ExecutionStatus,
-                    BarYLocation = (yLocation_ - BarHeight / 2).ToString().Replace(',', '.'),
-                    BarYLocationCollapsed = (yLocationCollapsed_ - BarHeightCollapsed / 2).ToString().Replace(',', '.'),
-                    BarXLocation = xLocation.ToString().Replace(',', '.'),
-                    BarWidth = width.ToString().Replace(',', '.')
-                });
-
             }
 
         }
@@ -232,24 +118,5 @@ namespace EtlManager.Pages.Executions
             return new JsonResult(new { success = true });
         }
 
-        public class ChartElement
-        {
-            public string StepExecutionId { get; set; }
-            public string ExecutionStatus { get; set; }
-            public string BarYLocation { get; set; }
-            public string BarYLocationCollapsed { get; set; }
-            public string BarXLocation { get; set; }
-            public string BarWidth { get; set; }
-        }
-
-        public class ChartLabel
-        {
-            public Guid StepId { get; set; }
-            public string StepName { get; set; }
-            public string LabelYLocation { get; set; }
-            public string LabelYLocationCollapsed { get; set; }
-            public double YLocationDouble { get; set; }
-            public double YLocationCollapsedDouble { get; set; }
-        }
     }
 }
