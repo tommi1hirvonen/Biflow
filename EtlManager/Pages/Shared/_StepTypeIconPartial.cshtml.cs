df14f93798fec1249d1edfdcf1c10a03ca175c92
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2020-12-03 21:51:54 +0200

    Created a partial view for step type icon

---
+++ EtlManager/Pages/Shared/_StepTypeIconPartial.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EtlManager.Pages.Shared
+{
+    public class _StepTypeIconPartialModel : PageModel
+    {
+        public string StepType { get; set; }
+    }
+}
