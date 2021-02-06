c97e32b881dc7b861eca3d5937889383f8dd2811
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2021-02-06 10:38:03 +0200

    Removed old Razor Pages UI project from solution

--- EtlManager/Pages/Shared/_StepTypeIconPartial.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace EtlManager.Pages.Shared
-{
-    public class _StepTypeIconPartialModel : PageModel
-    {
-        public string StepType { get; set; }
-    }
-}
