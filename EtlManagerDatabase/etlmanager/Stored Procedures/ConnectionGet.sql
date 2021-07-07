e47d05d85bbef417fd584133ba72c17d414c135b
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2021-07-07 19:14:00 +0300

    The user can toggle to show sensitive connection strings in ConnectionsComponent

---
+++ EtlManagerDatabase/etlmanager/Stored Procedures/ConnectionGet.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [etlmanager].[ConnectionGet]
+	@ConnectionId UNIQUEIDENTIFIER = NULL,
+	@EncryptionKey NVARCHAR(128) = NULL
+AS
+
+SELECT
+	a.ConnectionId,
+	a.ConnectionName,
+	ConnectionString =
+		CASE a.IsSensitive
+			WHEN 1 THEN etlmanager.GetConnectionStringDecrypted(a.ConnectionId, @EncryptionKey)
+			ELSE a.ConnectionString
+		END,
+	a.ExecutePackagesAsLogin,
+	a.IsSensitive
+FROM etlmanager.Connection AS a
+WHERE ISNULL(@ConnectionId, a.ConnectionId) = a.ConnectionId
