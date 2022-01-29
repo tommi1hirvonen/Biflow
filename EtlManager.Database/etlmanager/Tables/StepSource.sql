fb40a4302126e6590ea39b9b467302e6fb74a748
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2022-01-29 13:48:57 +0200

    Added on delete cascade to StepSource and StepTarget foreign key definitions

--- EtlManager.Database/etlmanager/Tables/StepSource.sql
+++ EtlManager.Database/etlmanager/Tables/StepSource.sql
@@ -3,6 +3,6 @@
 	[StepId] UNIQUEIDENTIFIER NOT NULL,
 	[DatabaseObjectId] UNIQUEIDENTIFIER NOT NULL,
 	CONSTRAINT [PK_StepSource] PRIMARY KEY CLUSTERED ([StepId], [DatabaseObjectId]),
-	CONSTRAINT [FK_StepSource_Step] FOREIGN KEY ([StepId]) REFERENCES [etlmanager].[Step] ([StepId]),
-	CONSTRAINT [FK_StepSource_DatabaseObject] FOREIGN KEY ([DatabaseObjectId]) REFERENCES [etlmanager].[DatabaseObject] ([DatabaseObjectId])
+	CONSTRAINT [FK_StepSource_Step] FOREIGN KEY ([StepId]) REFERENCES [etlmanager].[Step] ([StepId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_StepSource_DatabaseObject] FOREIGN KEY ([DatabaseObjectId]) REFERENCES [etlmanager].[DatabaseObject] ([DatabaseObjectId]) ON DELETE CASCADE
 )
--- EtlManager.Database/etlmanager/Tables/StepTarget.sql
+++ EtlManager.Database/etlmanager/Tables/StepTarget.sql
@@ -3,6 +3,6 @@
 	[StepId] UNIQUEIDENTIFIER NOT NULL,
 	[DatabaseObjectId] UNIQUEIDENTIFIER NOT NULL,
 	CONSTRAINT [PK_StepTarget] PRIMARY KEY CLUSTERED ([StepId], [DatabaseObjectId]),
-	CONSTRAINT [FK_StepTarget_Step] FOREIGN KEY ([StepId]) REFERENCES [etlmanager].[Step] ([StepId]),
-	CONSTRAINT [FK_StepTarget_DatabaseObject] FOREIGN KEY ([DatabaseObjectId]) REFERENCES [etlmanager].[DatabaseObject] ([DatabaseObjectId])
+	CONSTRAINT [FK_StepTarget_Step] FOREIGN KEY ([StepId]) REFERENCES [etlmanager].[Step] ([StepId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_StepTarget_DatabaseObject] FOREIGN KEY ([DatabaseObjectId]) REFERENCES [etlmanager].[DatabaseObject] ([DatabaseObjectId]) ON DELETE CASCADE
 )
