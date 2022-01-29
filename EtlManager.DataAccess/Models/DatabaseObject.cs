b53578be4885def0f1adc4020642ad98481d9a9d
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2022-01-29 18:23:00 +0200

    Added step source and target highlighting depending on whether the object is a candidate for removal or a new addition

--- EtlManager.DataAccess/Models/DatabaseObject.cs
+++ EtlManager.DataAccess/Models/DatabaseObject.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EtlManager.DataAccess.Models;
 
@@ -27,17 +28,41 @@ public class DatabaseObject
     [MaxLength(128)]
     public string ObjectName { get; set; } = string.Empty;
 
+    [NotMapped]
+    public bool IsCandidateForRemoval { get; set; } = false;
+
+    [NotMapped]
+    public bool IsNewAddition { get; set; } = false;
+
     public ICollection<Step> Targets { get; set; } = null!;
 
     public ICollection<Step> Sources { get; set; } = null!;
 
     public override bool Equals(object? obj)
     {
-        var dbo = obj as DatabaseObject;
-        return ServerName.Equals(dbo?.ServerName)
-            && DatabaseName.Equals(dbo.DatabaseName)
-            && SchemaName.Equals(dbo.SchemaName)
-            && ObjectName.Equals(dbo.ObjectName);
+        if (obj is DatabaseObject dbo)
+        {
+            return ServerName.Equals(dbo.ServerName)
+                && DatabaseName.Equals(dbo.DatabaseName)
+                && SchemaName.Equals(dbo.SchemaName)
+                && ObjectName.Equals(dbo.ObjectName);
+        }
+        else if (obj is ValueTuple<string, string, string, string> vt)
+        {
+            return ServerName.Equals(vt.Item1)
+                && DatabaseName.Equals(vt.Item2)
+                && SchemaName.Equals(vt.Item3)
+                && ObjectName.Equals(vt.Item4);
+        }
+        else if (obj is Tuple<string, string, string, string> tuple)
+        {
+            return ServerName.Equals(tuple.Item1)
+                && DatabaseName.Equals(tuple.Item2)
+                && SchemaName.Equals(tuple.Item3)
+                && ObjectName.Equals(tuple.Item4);
+        }
+
+        return false;
     }
 
     public override int GetHashCode() => HashCode.Combine(ServerName, DatabaseName, SchemaName, ObjectName);
