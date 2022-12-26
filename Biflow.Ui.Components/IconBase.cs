3628addfcf954d1b8f57e9be7ae4cc30d287e755
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2022-12-26 14:42:40 +0200

    Added source generation to generate statically typed icon classes

---
+++ Biflow.Ui.Components/IconBase.cs
@@ -0,0 +1,6 @@
+﻿namespace Biflow.Ui.Components;
+
+public abstract class IconBase
+{
+    public string? SvgText { get; protected set; }
+}
---
+++ Biflow.Ui.SourceGeneration/FeatherIconSourceGenerator.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Biflow.Ui.SourceGeneration
+{
+    [Generator]
+    internal class FeatherIconSourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            foreach (var tree in context.Compilation.SyntaxTrees)
+            {
+                if (Path.GetFileNameWithoutExtension(tree.FilePath) != "CxIcon")
+                {
+                    continue;
+                }
+
+                var sourceBuilder = new StringBuilder(@"
+using System;
+namespace Biflow.Ui.Components
+{
+    public class FeatherIcon : IconBase
+    {
+        private FeatherIcon(string svgText)
+        {
+            SvgText = svgText;
+        }
+");
+
+                var iconsDirectory = Path.Combine(Path.GetDirectoryName(tree.FilePath), "wwwroot", "icons", "feather");
+                var svgFiles = Directory.GetFiles(iconsDirectory, "*.svg");
+
+                foreach (var file in svgFiles)
+                {
+                    var svgText = File.ReadAllText(file);
+                    var iconName = Path.GetFileNameWithoutExtension(file);
+                    var propertyName = GetPropertyNameFromIconName(iconName);
+
+                    sourceBuilder.AppendLine($@"
+public static FeatherIcon {propertyName} => new FeatherIcon(""""""
+{svgText}
+"""""");");
+                }
+
+                sourceBuilder.Append(@"
+    }
+}");
+
+                context.AddSource("FeatherIcon.g.cs", sourceBuilder.ToString());
+
+            }
+
+        }
+
+        private string GetPropertyNameFromIconName(string iconName)
+        {
+            string[] segments = iconName.Split('-');
+            var propertyName = string.Join("", segments.Select(segment => segment.Substring(0, 1).ToUpper() + segment.Substring(1)));
+            if (!SyntaxFacts.IsValidIdentifier(propertyName))
+            {
+                // e.g. "123" => "_123"
+                propertyName = "_" + propertyName;
+            }
+            return propertyName;
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+    }
+}
