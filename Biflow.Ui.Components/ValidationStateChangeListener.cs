9451625d2707b0da34555a05f2f4b886e2791728
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-01-28 13:52:45 +0200

    [Ui] Added validation error message to step edit modal footer

---
+++ Biflow.Ui.Components/ValidationStateChangeListener.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Biflow.Ui.Components;
+
+public class ValidationStateChangeListener : ComponentBase, IDisposable
+{
+    [CascadingParameter] public EditContext? CurrentEditContext { get; set; }
+
+    [Parameter] public Action<IEnumerable<string>>? OnValidationStateChanged { get; set; }
+
+    private Action<IEnumerable<string>>? _previousOnValidationStateChanged;
+    private EditContext? _previousEditContext;
+    private EventHandler<ValidationStateChangedEventArgs>? _validationStateChangedHandler;
+
+    protected override void OnParametersSet()
+    {
+        if (CurrentEditContext is null)
+        {
+            throw new InvalidOperationException($"{nameof(ValidationStateChangeListener)} requires a cascading parameter " +
+                $"of type {nameof(EditContext)}. For example, you can use {nameof(ValidationStateChangeListener)} inside " +
+                $"an {nameof(EditForm)}.");
+        }
+
+        if (CurrentEditContext != _previousEditContext)
+        {
+            DetachValidationStateChangedListener();
+            _previousEditContext = CurrentEditContext;
+        }
+
+        if (OnValidationStateChanged != _previousOnValidationStateChanged)
+        {
+            _validationStateChangedHandler = (sender, eventArgs) => OnValidationStateChanged?.Invoke(CurrentEditContext.GetValidationMessages());
+            CurrentEditContext.OnValidationStateChanged += _validationStateChangedHandler;
+            _previousOnValidationStateChanged = OnValidationStateChanged;
+        }
+    }
+
+    public void Dispose()
+    {
+        DetachValidationStateChangedListener();
+    }
+
+    private void DetachValidationStateChangedListener()
+    {
+        if (_previousEditContext is not null)
+        {
+            _previousEditContext.OnValidationStateChanged -= _validationStateChangedHandler;
+        }
+    }
+}
