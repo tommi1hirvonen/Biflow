2b5877ae1fdf4777df0adaddc1f83fc5e85f1081
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-03-26 00:25:25 +0200

    Added unit tests for common utility project

---
+++ Biflow.Utilities.Test/CommonUtilityTests.cs
@@ -0,0 +1,17 @@
+namespace Biflow.Utilities.Test;
+
+public class CommonUtilityTests
+{
+    [Theory]
+    [InlineData(18000.5, "5 h 0 min 0 s")]
+    [InlineData(172800.5, "2 d 0 h 0 min 0 s")]
+    [InlineData(96001, "1 d 2 h 40 min 1 s")]
+    [InlineData(65.5, "1 min 5 s")]
+    [InlineData(45, "45 s")]
+    [InlineData(0.5, "0 s")]
+    public void SecondsToReadableFormatTest(double seconds, string expectedOutput)
+    {
+        string result = seconds.SecondsToReadableFormat();
+        Assert.Equal(expectedOutput, result);
+    }
+}
