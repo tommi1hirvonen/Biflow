cd112ccd7e2ee17452e4db2423e057eecef3983e
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-08-10 14:11:28 +0300

    [Ui.Core] [Column] Introduce IsNullable property

--- Biflow.Ui.Core/TableEditor/Column.cs
+++ Biflow.Ui.Core/TableEditor/Column.cs
@@ -13,4 +13,6 @@ public record Column(
     Lookup? Lookup)
 {
     public bool IsEditable => !IsIdentity && !IsComputed && Datatype is not null;
+
+    public bool IsNullable => !DbDatatype.ContainsIgnoreCase("not null");
 }
