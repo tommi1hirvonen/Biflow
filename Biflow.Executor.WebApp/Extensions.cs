19a026e595ad1236e5ac3c2f7cc04b011ec253e1
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-09-17 22:39:47 +0300

    [Executor.WebApp] Moved endpoint mapping to public extension method

---
+++ Biflow.Executor.WebApp/Extensions.cs
@@ -0,0 +1,51 @@
+﻿using Biflow.Executor.Core.ConnectionTest;
+using Biflow.Executor.Core.Notification;
+using Biflow.Executor.Core.WebExtensions;
+
+namespace Biflow.Executor.WebApp;
+
+public static class Extensions
+{
+    public static WebApplication MapExecutorEndpoints(this WebApplication app)
+    {
+        app.MapGet("/execution/start/{executionId}", async (Guid executionId, IExecutionManager executionManager) =>
+        {
+            await executionManager.StartExecutionAsync(executionId);
+        }).WithName("StartExecution");
+
+
+        app.MapGet("/execution/stop/{executionId}", (Guid executionId, string username, IExecutionManager executionManager) =>
+        {
+            executionManager.CancelExecution(executionId, username);
+        }).WithName("StopExecution");
+
+
+        app.MapGet("/execution/stop/{executionId}/{stepId}", (Guid executionId, Guid stepId, string username, IExecutionManager executionManager) =>
+        {
+            executionManager.CancelExecution(executionId, username, stepId);
+        }).WithName("StopExecutionStep");
+
+
+        app.MapGet("/execution/status/{executionId}", (Guid executionId, IExecutionManager executionManager) =>
+        {
+            return executionManager.IsExecutionRunning(executionId)
+                ? Results.Ok()
+                : Results.NotFound();
+        }).WithName("ExecutionStatus");
+
+
+        app.MapGet("/connection/test", async (IConnectionTest connectionTest) =>
+        {
+            await connectionTest.RunAsync();
+        }).WithName("TestConnection");
+
+
+        app.MapPost("/email/test", async (string address, IEmailTest emailTest) =>
+        {
+            await emailTest.RunAsync(address);
+            return Results.Ok("Email test succeeded. Check that the email was received successfully.");
+        }).WithName("TestEmail");
+
+        return app;
+    }
+}
--- Biflow.Executor.WebApp/Program.cs
+++ Biflow.Executor.WebApp/Program.cs
@@ -1,7 +1,5 @@
 using Biflow.Executor.Core;
-using Biflow.Executor.Core.ConnectionTest;
-using Biflow.Executor.Core.Notification;
-using Biflow.Executor.Core.WebExtensions;
+using Biflow.Executor.WebApp;
 using Microsoft.Extensions.Hosting.WindowsServices;
 using Serilog;
 
@@ -32,44 +30,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
-app.MapGet("/execution/start/{executionId}", async (Guid executionId, IExecutionManager executionManager) =>
-{
-    await executionManager.StartExecutionAsync(executionId);
-}).WithName("StartExecution");
-
-
-app.MapGet("/execution/stop/{executionId}", (Guid executionId, string username, IExecutionManager executionManager) =>
-{
-    executionManager.CancelExecution(executionId, username);
-}).WithName("StopExecution");
-
-
-app.MapGet("/execution/stop/{executionId}/{stepId}", (Guid executionId, Guid stepId, string username, IExecutionManager executionManager) =>
-{
-    executionManager.CancelExecution(executionId, username, stepId);
-}).WithName("StopExecutionStep");
-
-
-app.MapGet("/execution/status/{executionId}", (Guid executionId, IExecutionManager executionManager) =>
-{
-    return executionManager.IsExecutionRunning(executionId)
-        ? Results.Ok()
-        : Results.NotFound();
-}).WithName("ExecutionStatus");
-
-
-app.MapGet("/connection/test", async (IConnectionTest connectionTest) =>
-{
-    await connectionTest.RunAsync();
-}).WithName("TestConnection");
-
-
-app.MapPost("/email/test", async (string address, IEmailTest emailTest) =>
-{
-    await emailTest.RunAsync(address);
-    return Results.Ok("Email test succeeded. Check that the email was received successfully.");
-}).WithName("TestEmail");
-
+app.MapExecutorEndpoints();
 
 app.Run();
