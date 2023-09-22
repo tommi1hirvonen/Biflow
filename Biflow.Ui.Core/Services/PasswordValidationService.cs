a001d2d2a000cf886e78c8569f49c14cbc889f42
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-09-22 21:56:25 +0300

    [Ui] [Account] Added password complexity validation

---
+++ Biflow.Ui.Core/Services/PasswordValidationService.cs
@@ -0,0 +1,7 @@
+﻿namespace Biflow.Ui.Core;
+
+public class PasswordValidationService
+{
+    public bool ValidatePassword(string password) =>
+        Extensions.PasswordValidationRegex().Matches(password).Any();
+}
