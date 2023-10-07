6b2f818543ca3a8b971025985e63016785480183
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-10-07 18:35:37 +0300

    [Ui] AppSelectOffcanvas to select Qlik app from list

--- Biflow.Ui/Shared/StepEditModal/QlikStepEditModal.razor.cs
+++ Biflow.Ui/Shared/StepEditModal/QlikStepEditModal.razor.cs
@@ -1,5 +1,7 @@
 ﻿using Biflow.DataAccess;
 using Biflow.DataAccess.Models;
+using Biflow.Ui.Core;
+using Biflow.Ui.Shared.StepEdit;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,8 +13,12 @@ public partial class QlikStepEditModal : StepEditModal<QlikStep>
 
     internal override string FormId => "qlik_step_edit_form";
 
+    private AppSelectOffcanvas? appSelectOffcanvas;
+    private string? appName;
+
     protected override async Task<QlikStep> GetExistingStepAsync(AppDbContext context, Guid stepId)
     {
+        appName = null;
         var step = await context.QlikSteps
             .Include(step => step.Job)
             .Include(step => step.Tags)
@@ -26,6 +32,7 @@ public partial class QlikStepEditModal : StepEditModal<QlikStep>
 
     protected override QlikStep CreateNewStep(Job job)
     {
+        appName = "";
         var client = Clients?.FirstOrDefault();
         ArgumentNullException.ThrowIfNull(client);
         return new(job.JobId, "")
@@ -35,11 +42,46 @@ public partial class QlikStepEditModal : StepEditModal<QlikStep>
             RetryIntervalMinutes = 0,
             TimeoutMinutes = 0,
             QlikCloudClientId = client.QlikCloudClientId,
+            QlikCloudClient = client,
             Dependencies = new List<Dependency>(),
             Tags = new List<Tag>(),
             Sources = new List<DataObject>(),
             Targets = new List<DataObject>(),
             ExecutionConditionParameters = new List<ExecutionConditionParameter>()
         };
+    }
+
+    protected override async Task OnModalShownAsync(QlikStep step)
+    {
+        try
+        {
+            var client = Clients?.FirstOrDefault(c => c.QlikCloudClientId == step.QlikCloudClientId);
+            appName = client is not null
+                ? await client.GetAppNameAsync(step.AppId)
+                : "";
+        }
+        catch
+        {
+            appName = "";
+        }
+        finally
+        {
+            StateHasChanged();
+        }
+    }
+
+    private void OnAppSelected(QlikApp selected)
+    {
+        ArgumentNullException.ThrowIfNull(Step);
+        Step.AppId = selected.Id;
+        appName = selected.Name;
+    }
+
+    private Task OpenAppSelectOffcanvas()
+    {
+        ArgumentNullException.ThrowIfNull(Step);
+        ArgumentNullException.ThrowIfNull(Clients);
+        var client = Clients.First(c => c.QlikCloudClientId == Step.QlikCloudClientId);
+        return appSelectOffcanvas.LetAsync(x => x.ShowAsync(client));
     }
 }
