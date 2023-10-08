269037030553e94c60058e0ba94b0b344992e1dd
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-10-08 11:59:17 +0300

    [DataAccess] [StepType] Added attributes to describe various step types

--- Biflow.DataAccess/Models/Step/StepType.cs
+++ Biflow.DataAccess/Models/Step/StepType.cs
@@ -1,16 +1,95 @@
-﻿namespace Biflow.DataAccess.Models;
+﻿using System.Reflection;
+
+namespace Biflow.DataAccess.Models;
 
 public enum StepType
 {
-    Sql,
+    [Category("Integration", 1)]
+    [Description("SQL Server Integration Services package")]
     Package,
+    
+    [Category("Integration", 1)]
+    [Description("Azure Data Factory or Synapse pipeline")]
     Pipeline,
+    
+    [Category("Integration", 1)]
+    [Description("Azure Function")]
     Function,
+    
+    [Category("Transformation", 2)]
+    [Description("Custom SQL command")]
+    Sql,
+    
+    [Category("Reporting", 3)]
+    [Description("SQL Server or Azure Analysis Services tabular model")]
     Tabular,
+    
+    [Category("Reporting", 3)]
+    [Description("Power BI dataset refresh")]
     Dataset,
+    
+    [Category("Reporting", 3)]
+    [Description("Qlik Cloud app reload")]
     Qlik,
+    
+    [Category("Other", 4)]
+    [Description("SQL Server Agent job")]
     AgentJob,
+    
+    [Category("Other", 4)]
+    [Description("Executable")]
     Exe,
-    Job,
-    Email
+    
+    [Category("Other", 4)]
+    [Description("Send custom email")]
+    Email,
+    
+    [Category("Other", 4)]
+    [Description("Execute another job")]
+    Job
+}
+
+public static class StepTypeExtensions
+{
+    public static (string Name, int Ordinal)? GetCategory(this StepType value)
+    {
+        var name = Enum.GetName(value);
+        if (name is null)
+        {
+            return null;
+        }
+        var category = typeof(StepType).GetField(name)?.GetCustomAttributes<CategoryAttribute>().FirstOrDefault();
+        return category is not null
+            ? (category.Name, category.Ordinal)
+            : null;
+    }
+
+    public static string? GetDescription(this StepType value)
+    {
+        var name = Enum.GetName(value);
+        if (name is null)
+        {
+            return null;
+        }
+        var description = typeof(StepType).GetField(name)?.GetCustomAttributes<DescriptionAttribute>().FirstOrDefault();
+        return description?.Text;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+file class CategoryAttribute : Attribute
+{
+    public CategoryAttribute(string name, int ordinal) => (Name, Ordinal) = (name, ordinal);
+
+    public string Name { get; }
+
+    public int Ordinal { get; }
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+file class DescriptionAttribute : Attribute
+{
+    public DescriptionAttribute(string text) => Text = text;
+
+    public string Text { get; }
 }
