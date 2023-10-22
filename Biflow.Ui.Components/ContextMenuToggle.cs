1bec42e413e31a4fb5987a4093a5e12dbe9ca16a
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-10-22 13:39:44 +0300

    [Ui.Components] [ContextMenuToggle] Attribute splatting

--- Biflow.Ui.Components/ContextMenuToggle.cs
+++ Biflow.Ui.Components/ContextMenuToggle.cs
@@ -8,20 +8,20 @@ public class ContextMenuToggle : ComponentBase
 {
     [Inject] private ContextMenuService ContextMenuService { get; set; } = null!;
 
+    [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object>? InputAttributes { get; set; }
+
     [Parameter] public string ContainerHtmlTag { get; set; } = "div";
 
     [Parameter] public RenderFragment? ChildContent { get; set; }
 
     [Parameter] public RenderFragment? MenuContent { get; set; }
 
-    [Parameter] public string? ContainerCssClass { get; set; }
-
     [Parameter] public bool Disabled { get; set; } = false;
 
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
         builder.OpenElement(0, ContainerHtmlTag);
-        builder.AddAttribute(1, "class", ContainerCssClass);
+        builder.AddMultipleAttributes(1, InputAttributes);
         builder.AddAttribute(2, "oncontextmenu", EventCallback.Factory.Create<MouseEventArgs>(this, ShowContextMenuAsync));
         builder.AddEventPreventDefaultAttribute(3, "oncontextmenu", true);
         builder.AddContent(4, ChildContent);
