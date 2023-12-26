75281c272a351e4a5930e856e019e8c4311a925b
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-12-26 19:56:39 +0200

    [DataAccess] Support for EF Core migrations

---
+++ Biflow.DataAccess/AppDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Biflow.DataAccess;
+
+internal class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var settings = new Dictionary<string, string?>
+        {
+            {
+                "ConnectionStrings:AppDbContext",
+                "Data Source=localhost;Database=Biflow;Integrated Security=sspi;Encrypt=true;TrustServerCertificate=true;"
+            }
+        };
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(settings)
+            .Build();
+        return new AppDbContext(configuration);
+    }
+}
