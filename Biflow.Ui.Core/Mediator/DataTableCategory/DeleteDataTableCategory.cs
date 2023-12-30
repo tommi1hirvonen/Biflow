49395bade2a39974ba9629b4c15c222cd2cbaaaf
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-12-30 17:28:11 +0200

    [Ui.Core] Mediator handles data table category create, update & delete

---
+++ Biflow.Ui.Core/Mediator/DataTableCategory/DeleteDataTableCategory.cs
@@ -0,0 +1,23 @@
+﻿using Biflow.DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Core;
+
+public record DeleteDataTableCategoryCommand(Guid CategoryId) : IRequest;
+
+internal class DeleteDataTableCategoryCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
+    : IRequestHandler<DeleteDataTableCategoryCommand>
+{
+    public async Task Handle(DeleteDataTableCategoryCommand request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        var category = await context.MasterDataTableCategories
+            .FirstOrDefaultAsync(c => c.CategoryId == request.CategoryId, cancellationToken);
+        if (category is not null)
+        {
+            context.MasterDataTableCategories.Remove(category);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Biflow.Ui.Core/Mediator/DataTableCategory/UpdateDataTableCategory.cs
@@ -0,0 +1,19 @@
+﻿using Biflow.DataAccess;
+using Biflow.DataAccess.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Core;
+
+public record UpdateDataTableCategoryCommand(MasterDataTableCategory Category) : IRequest;
+
+internal class UpdateDataTableCategoryCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
+    : IRequestHandler<UpdateDataTableCategoryCommand>
+{
+    public async Task Handle(UpdateDataTableCategoryCommand request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        context.MasterDataTableCategories.Attach(request.Category).State = EntityState.Modified;
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
