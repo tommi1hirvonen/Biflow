0a504358602c0570ae7442a4be148ab0d45f97dc
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-12-30 18:39:29 +0200

    [Ui.Core] Mediator handles dependency create and delete

---
+++ Biflow.Ui.Core/Mediator/Dependency/DeleteDependency.cs
@@ -0,0 +1,20 @@
+﻿using Biflow.DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Core;
+
+public record DeleteDependencyCommand(Guid StepId, Guid DependentOnStepId) : IRequest;
+
+internal class DeleteDependencyCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
+    : IRequestHandler<DeleteDependencyCommand>
+{
+    public async Task Handle(DeleteDependencyCommand request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        await context.Dependencies
+            .Where(d => d.StepId == request.StepId && d.DependantOnStepId == request.DependentOnStepId)
+            .ExecuteDeleteAsync(cancellationToken);
+
+    }
+}
