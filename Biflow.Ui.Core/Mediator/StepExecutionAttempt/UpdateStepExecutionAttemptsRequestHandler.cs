6cfb08a0218ff3abba252b711d82ee7ab762bce3
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-12-30 12:53:59 +0200

    [Ui.Core] Mediator handles step execution attempt updates

---
+++ Biflow.Ui.Core/Mediator/StepExecutionAttempt/UpdateStepExecutionAttemptsRequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using Biflow.DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Core;
+
+internal class UpdateStepExecutionAttemptsRequestHandler(IDbContextFactory<AppDbContext> dbContextFactory)
+    : IRequestHandler<UpdateStepExecutionAttemptsRequest>
+{
+    public async Task Handle(UpdateStepExecutionAttemptsRequest request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        foreach (var attempt in request.Attempts)
+        {
+            context.Attach(attempt).State = EntityState.Modified;
+        }
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
