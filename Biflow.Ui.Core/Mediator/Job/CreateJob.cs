02f3dd4375137962bb6963ebcdc0180771abc109
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-12-30 18:28:35 +0200

    [Ui.Core] Mediator handles job create and update

---
+++ Biflow.Ui.Core/Mediator/Job/CreateJob.cs
@@ -0,0 +1,19 @@
+﻿using Biflow.DataAccess;
+using Biflow.DataAccess.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Core;
+
+public record CreateJobCommand(Job Job) : IRequest;
+
+internal class CreateJobCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
+    : IRequestHandler<CreateJobCommand>
+{
+    public async Task Handle(CreateJobCommand request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        context.Jobs.Add(request.Job);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ Biflow.Ui.Core/Mediator/Job/UpdateJob.cs
@@ -0,0 +1,18 @@
+﻿using Biflow.DataAccess;
+using Biflow.DataAccess.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Core;
+
+public record UpdateJobCommand(Job Job) : IRequest;
+
+internal class UpdateJobCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory) : IRequestHandler<UpdateJobCommand>
+{
+    public async Task Handle(UpdateJobCommand request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        context.Jobs.Attach(request.Job).State = EntityState.Modified;
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
