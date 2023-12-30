23a186bcc8fc87af9e2e8a4f0024f65e20e21681
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-12-30 16:06:04 +0200

    [Ui.Core] Mediator handles subscription create, update & delete

---
+++ Biflow.Ui.Core/Mediator/Subscription/UpdateSubscription.cs
@@ -0,0 +1,19 @@
+﻿using Biflow.DataAccess;
+using Biflow.DataAccess.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Core;
+
+public record UpdateSubscriptionCommand(Subscription Subscription) : IRequest;
+
+internal class UpdateSubscriptionCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
+    : IRequestHandler<UpdateSubscriptionCommand>
+{
+    public async Task Handle(UpdateSubscriptionCommand request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        context.Attach(request.Subscription).State = EntityState.Modified;
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
