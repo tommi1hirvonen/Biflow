45ee3c30cb2cb58f9d585eca9f308572b1d07cfc
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-12-30 16:36:25 +0200

    [Ui.Core] Mediator handles data object update

---
+++ Biflow.Ui.Core/Mediator/DataObject/UpdateDataObject.cs
@@ -0,0 +1,19 @@
+﻿using Biflow.DataAccess;
+using Biflow.DataAccess.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Core;
+
+public record UpdateDataObjectCommand(DataObject DataObject) : IRequest;
+
+internal class UpdateDataObjectCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
+    : IRequestHandler<UpdateDataObjectCommand>
+{
+    public async Task Handle(UpdateDataObjectCommand request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        context.Attach(request.DataObject).State = EntityState.Modified;
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
