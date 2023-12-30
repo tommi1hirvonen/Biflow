4e2d17fbd6e00d10b90cbeabf88fd817c35155e3
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-12-30 15:26:50 +0200

    [Ui.Core] Mediator handles function app create, update & delete

---
+++ Biflow.Ui.Core/Mediator/FunctionApp/DeleteFunctionApp.cs
@@ -0,0 +1,23 @@
+﻿using Biflow.DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Core;
+
+public record DeleteFunctionAppCommand(Guid FunctionAppId) : IRequest;
+
+internal class DeleteFunctionAppCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
+    : IRequestHandler<DeleteFunctionAppCommand>
+{
+    public async Task Handle(DeleteFunctionAppCommand request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        var client = await context.FunctionApps
+            .FirstOrDefaultAsync(p => p.FunctionAppId == request.FunctionAppId, cancellationToken);
+        if (client is not null)
+        {
+            context.FunctionApps.Remove(client);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
