a921bc21a0295b2cdcb29a918447989860a6d7e6
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-12-30 20:03:39 +0200

    [Ui.Core] Separate commands and handlers just for job concurrencies and parameters

---
+++ Biflow.Ui.Core/Mediator/Job/UpdateJobConcurrencies.cs
@@ -0,0 +1,26 @@
+﻿using Biflow.DataAccess;
+using Biflow.DataAccess.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Core;
+
+public record UpdateJobConcurrenciesCommand(Job Job) : IRequest;
+
+internal class UpdateJobConcurrenciesCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
+    : IRequestHandler<UpdateJobConcurrenciesCommand>
+{
+    public async Task Handle(UpdateJobConcurrenciesCommand request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        var job = await context.Jobs
+            .Include(j => j.JobConcurrencies)
+            .FirstOrDefaultAsync(j => j.JobId == request.Job.JobId, cancellationToken);
+        if (job is null)
+        {
+            return;
+        }
+        context.MergeCollections(job.JobConcurrencies, request.Job.JobConcurrencies, c => c.StepType);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
