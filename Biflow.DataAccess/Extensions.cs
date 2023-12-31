c9d23dc172216d0a0eb383e4474fa49bffa085ee
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2023-12-31 19:02:20 +0200

    [DataAccess] [Extensions] Extension method for retrieving execution with entire nav graph

--- Biflow.DataAccess/Extensions.cs
+++ Biflow.DataAccess/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Biflow.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Biflow.DataAccess;
 
@@ -18,6 +20,142 @@ public static class Extensions
         return services;
     }
 
+    /// <summary>
+    /// Query execution and include the entire navigation graph. This includes step executions, attempts and parameters etc.
+    /// Step execution endpoint clients are also included (e.g. <see cref="SqlStepExecution.GetConnection"/>).
+    /// </summary>
+    /// <param name="context"><see cref="AppDbContext"/> instance to query</param>
+    /// <param name="executionId">id of the execution to get</param>
+    /// <param name="includeEndpoints">Include step execution endpoints (e.g. connections) so that calls such as <see cref="SqlStepExecution.GetConnection"/>
+    /// return the current endpoint if it has not been deleted from the db</param>
+    /// <param name="includeStep">Include step navigation so that calling <see cref="StepExecution.GetStep"/>
+    /// returns the current step if it has not been deleted from the db</param>
+    /// <returns><see cref="Execution"/> with all navigation properties included (incl. step execution endpoint clients, e.g. connections)</returns>
+    /// <exception cref="ExecutionNotFoundException"><see cref="Execution"/> with the given execution id was not found</exception>
+    public static async Task<Execution> GetExecutionWithEntireGraphAsync(
+        this AppDbContext context,
+        Guid executionId,
+        bool includeEndpoint = false,
+        bool includeStep = false)
+    {
+        // Use tracking queries and let EF match the entities from the two separate queries.
+
+        var execution = await context.Executions
+            .Include(e => e.ExecutionParameters)
+            .Include(e => e.ExecutionConcurrencies)
+            .FirstOrDefaultAsync(e => e.ExecutionId == executionId)
+            ?? throw new ExecutionNotFoundException(executionId);
+
+        var query1 = context.StepExecutions
+            .Where(e => e.ExecutionId == executionId)
+            .Include(e => e.StepExecutionAttempts)
+            .Include(e => e.ExecutionDependencies)
+            .ThenInclude(e => e.DependantOnStepExecution)
+            .Include(e => e.DataObjects)
+            .ThenInclude(s => s.DataObject)
+            .Include(e => e.ExecutionConditionParameters)
+            .ThenInclude(e => e.ExecutionParameter)
+            .Include($"{nameof(IHasStepExecutionParameters.StepExecutionParameters)}.{nameof(StepExecutionParameterBase.InheritFromExecutionParameter)}")
+            .Include($"{nameof(IHasStepExecutionParameters.StepExecutionParameters)}.{nameof(StepExecutionParameterBase.ExpressionParameters)}");
+
+        var sqlConnections = context.SqlConnections;
+        var asConnections = context.AnalysisServicesConnections;
+        var appRegistrations = context.AppRegistrations;
+        var functionApps = context.FunctionApps;
+        var pipelineClients = context.PipelineClients.Include(a => a.AppRegistration);
+        var qlikClients = context.QlikCloudClients;
+        var steps = context.Steps;
+
+        // Left join endpoint clients to step executions.
+        var query2 =
+            from stepExec in query1
+            join sql in sqlConnections on new { Id = includeEndpoint ? (object?)((SqlStepExecution)stepExec).ConnectionId : true } equals new { Id = includeStep ? (object?)sql.ConnectionId : false } into sql_
+            from sql in sql_.DefaultIfEmpty()
+            join package in sqlConnections on new { Id = includeEndpoint ? (object?)((PackageStepExecution)stepExec).ConnectionId : true } equals new { Id = includeStep ? (object?)package.ConnectionId : false } into package_
+            from package in package_.DefaultIfEmpty()
+            join agent in sqlConnections on new { Id = includeEndpoint ? (object?)((AgentJobStepExecution)stepExec).ConnectionId : true } equals new { Id = includeStep ? (object?)agent.ConnectionId : false } into agent_
+            from agent in agent_.DefaultIfEmpty()
+            join tabular in asConnections on new { Id = includeEndpoint ? (object?)((TabularStepExecution)stepExec).ConnectionId : true } equals new { Id = includeStep ? (object?)tabular.ConnectionId : false } into tabular_
+            from tabular in tabular_.DefaultIfEmpty()
+            join dataset in appRegistrations on new { Id = includeEndpoint ? (object?)((DatasetStepExecution)stepExec).AppRegistrationId : true } equals new { Id = includeStep ? (object?)dataset.AppRegistrationId : false } into dataset_
+            from dataset in dataset_.DefaultIfEmpty()
+            join function in functionApps on new { Id = includeEndpoint ? (object?)((FunctionStepExecution)stepExec).FunctionAppId : true } equals new { Id = includeStep ? (object?)function.FunctionAppId : false } into function_
+            from function in function_.DefaultIfEmpty()
+            join pipeline in pipelineClients on new { Id = includeEndpoint ? (object?)((PipelineStepExecution)stepExec).PipelineClientId : true } equals new { Id = includeStep ? (object?)pipeline.PipelineClientId : false } into pipeline_
+            from pipeline in pipeline_.DefaultIfEmpty()
+            join qlik in qlikClients on new { Id = includeEndpoint ? (object?)((QlikStepExecution)stepExec).QlikCloudClientId : true } equals new { Id = includeStep ? (object?)qlik.QlikCloudClientId : false } into qlik_
+            from qlik in qlik_.DefaultIfEmpty()
+            join step in steps on new { Id = includeStep ? (object?)stepExec.StepId : true } equals new { Id = includeStep ? (object?)step.StepId : false } into step_
+            from step in step_.DefaultIfEmpty()
+            select new StepExecutionProjection(
+                stepExec,
+                sql,
+                package,
+                agent,
+                tabular,
+                dataset,
+                function,
+                pipeline,
+                qlik,
+                step);
+
+        var stepExecutions = await query2.ToArrayAsync();
+
+        // Map endpoint clients to step executions.
+        foreach (var step in stepExecutions)
+        {
+            step.StepExecution.SetStep(step.Step);
+            switch (step.StepExecution)
+            {
+                case SqlStepExecution sql:
+                    sql.SetConnection(step.SqlStepConnection);
+                    break;
+                case PackageStepExecution package:
+                    package.SetConnection(step.PackageStepConnection);
+                    break;
+                case AgentJobStepExecution agent:
+                    agent.SetConnection(step.AgentJobStepConnection);
+                    break;
+                case TabularStepExecution tabular:
+                    tabular.SetConnection(step.TabularStepConnection);
+                    break;
+                case DatasetStepExecution dataset:
+                    dataset.SetAppRegistration(step.DatasetStepAppRegistration);
+                    break;
+                case FunctionStepExecution function:
+                    function.SetApp(step.FunctionStepApp);
+                    break;
+                case PipelineStepExecution pipeline:
+                    pipeline.SetClient(step.PipelineStepClient);
+                    break;
+                case QlikStepExecution qlik:
+                    qlik.SetClient(step.QlikStepClient);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return execution;
+    }
+
     internal static bool EqualsIgnoreCase(this string text, string? compareTo) =>
         string.Equals(text, compareTo, StringComparison.OrdinalIgnoreCase);
 }
+
+internal class ExecutionNotFoundException(Guid executionId) : Exception($"No execution found for id {executionId}")
+{
+    public Guid ExecutionId { get; } = executionId;
+}
+
+file record StepExecutionProjection(
+    StepExecution StepExecution,
+    SqlConnectionInfo? SqlStepConnection,
+    SqlConnectionInfo? PackageStepConnection,
+    SqlConnectionInfo? AgentJobStepConnection,
+    AnalysisServicesConnectionInfo? TabularStepConnection,
+    AppRegistration? DatasetStepAppRegistration,
+    FunctionApp? FunctionStepApp,
+    PipelineClient? PipelineStepClient,
+    QlikCloudClient? QlikStepClient,
+    Step? Step);
