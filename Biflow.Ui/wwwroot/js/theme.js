e532c567c411bb79d2391074a3fcac9f499ad1a8
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-01-11 21:10:32 +0200

    [Ui] Improved how theme is applied in login page

--- Biflow.Ui/wwwroot/js/theme.js
+++ Biflow.Ui/wwwroot/js/theme.js
@@ -1,56 +1,22 @@
-﻿const storedTheme = localStorage.getItem('theme')
+﻿(() => {
+    const getStoredTheme = () => localStorage.getItem('theme')
 
-function getPreferredTheme() {
-    if (storedTheme) {
-        return storedTheme;
+    const getPreferredTheme = () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme) {
+            return storedTheme
+        }
+
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
     }
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-}
 
-function setTheme(theme) {
-    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.documentElement.setAttribute('data-bs-theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-bs-theme', theme);
+    const setTheme = theme => {
+        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme)
+        }
     }
-}
 
-(() => {
-    setTheme(getPreferredTheme());
+    setTheme(getPreferredTheme())
 })();
-
-function showActiveTheme(theme) {
-    // Activate the correct theme toggle dropdown item.
-    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
-    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-        element.classList.remove('active');
-    });
-    btnToActive.classList.add('active');
-
-    // Show the correct dropdown button icon.
-    const iconToShow = document.getElementById(`theme_dropdown_icon_${theme}`);
-    document.querySelectorAll('.theme-toggle-dropdown-icon').forEach(element => {
-        element.classList.add('d-none');
-    });
-    iconToShow.classList.remove('d-none');
-}
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    if (storedTheme !== 'light' || storedTheme !== 'dark') {
-        setTheme(getPreferredTheme());
-    }
-})
-
-function addThemeToggleListeners() {
-    showActiveTheme(getPreferredTheme());
-
-    document.querySelectorAll('[data-bs-theme-value]')
-        .forEach(toggle => {
-            toggle.addEventListener('click', () => {
-                const theme = toggle.getAttribute('data-bs-theme-value');
-                localStorage.setItem('theme', theme);
-                setTheme(theme);
-                showActiveTheme(theme);
-            })
-        });
-}
