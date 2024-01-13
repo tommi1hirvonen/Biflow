64c2d624209afabe9656b348e48622856897f8e0
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-01-13 16:48:30 +0200

    [Scheduler.Core] Fix build errors

--- Biflow.Scheduler.Core/SchedulerDbContext.cs
+++ Biflow.Scheduler.Core/SchedulerDbContext.cs
@@ -1,13 +1,12 @@
-﻿using Biflow.DataAccess;
-using Biflow.DataAccess.Models;
+﻿using Biflow.Core.Entities;
+using Biflow.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 
 namespace Biflow.Executor.Core;
 
-public class SchedulerDbContext(IConfiguration configuration, IHttpClientFactory httpClientFactory, ITokenService tokenService)
-    : AppDbContext(configuration, httpClientFactory, tokenService, httpContextAccessor: null)
+public class SchedulerDbContext(IConfiguration configuration) : AppDbContext(configuration, httpContextAccessor: null)
 {
     protected override void ConfigureSqlServer(SqlServerDbContextOptionsBuilder options)
     {
