67174d9865dd6a94c8ab0acf4c8b910637dfa731
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-01-14 17:12:24 +0200

    [Core] Changed step param expression param collection nav type and accessiblity

--- Biflow.Ui.Core/Validation/StepParametersValidator.cs
+++ Biflow.Ui.Core/Validation/StepParametersValidator.cs
@@ -11,7 +11,7 @@ public class StepParametersValidator : AsyncAbstractValidator<IHasStepParameters
             .Must(step => step.StepParameters.DistinctBy(p => p.DisplayName).Count() == step.StepParameters.Count)
             .WithMessage("Parameter names must be unique");
         RuleForEach(step => step.StepParameters)
-            .Must(parameters => parameters.ExpressionParameters.DistinctBy(p => p.ParameterName).Count() == parameters.ExpressionParameters.Count)
+            .Must(parameters => parameters.ExpressionParameters.DistinctBy(p => p.ParameterName).Count() == parameters.ExpressionParameters.Count())
             .WithMessage("Expression parameter names must be unique");
         RuleForEach(step => step.StepParameters)
             .CustomAsync(async (param, context, ct) =>
