06151f9d981ca958706107d956b4e3fd9a169431
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-01-20 13:04:06 +0200

    [Ui.Core] [Mediator] Using reflection in SendAsync() without response type

--- Biflow.Ui.Core/Mediator/Mediator.cs
+++ Biflow.Ui.Core/Mediator/Mediator.cs
@@ -9,8 +9,17 @@ internal class Mediator(IServiceProvider serviceProvider) : IMediator
     public Task SendAsync<TRequest>(TRequest request, CancellationToken cancellationToken = default)
         where TRequest : IRequest
     {
-        var handler = _serviceProvider.GetRequiredService<IRequestHandler<TRequest>>();
-        return handler.Handle(request, cancellationToken);
+        // Create a generic MethodInfo to be used with the request handler from DI.
+        var genericType = typeof(IRequestHandler<>).MakeGenericType(request.GetType());
+        var handleMethod = genericType.GetMethod(nameof(IRequestHandler<TRequest>.Handle));
+        ArgumentNullException.ThrowIfNull(handleMethod);
+
+        // Get the corresponding handler for the request.
+        var handler = _serviceProvider.GetRequiredService(genericType);
+        var task = handleMethod.Invoke(handler, parameters: [request, cancellationToken]);
+        ArgumentNullException.ThrowIfNull(task);
+
+        return (Task)task;
     }
 
     public Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
