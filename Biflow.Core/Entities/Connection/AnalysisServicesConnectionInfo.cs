52fb31e6a07f507d72bbcc1256885995bcaadd29
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-01-20 22:46:37 +0200

    [Core] Changed connection nav collection type and accessibility

--- Biflow.Core/Entities/Connection/AnalysisServicesConnectionInfo.cs
+++ Biflow.Core/Entities/Connection/AnalysisServicesConnectionInfo.cs
@@ -5,7 +5,7 @@ namespace Biflow.Core.Entities;
 public class AnalysisServicesConnectionInfo() : ConnectionInfoBase(ConnectionType.AnalysisServices)
 {
     [JsonIgnore]
-    public IList<TabularStep> TabularSteps { get; set; } = null!;
+    public IEnumerable<TabularStep> TabularSteps { get; } = new List<TabularStep>();
 
     [JsonIgnore]
     public override IEnumerable<Step> Steps => TabularSteps?.Cast<Step>() ?? Enumerable.Empty<Step>();
