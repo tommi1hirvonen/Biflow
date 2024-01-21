8fa69fa09dbe943e7a3022e814ed4345dfabd32f
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-01-21 08:54:16 +0200

    [Core] [FunctionApp] App registration id has private setter

--- Biflow.Ui.Core/Mediator/Commands/FunctionApp/CreateFunctionApp.cs
+++ Biflow.Ui.Core/Mediator/Commands/FunctionApp/CreateFunctionApp.cs
@@ -9,6 +9,7 @@ internal class CreateFunctionAppCommandHandler(IDbContextFactory<AppDbContext> d
     {
         using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
         context.FunctionApps.Add(request.FunctionApp);
+        context.Entry(request.FunctionApp.AppRegistration).State = EntityState.Unchanged;
         await context.SaveChangesAsync(cancellationToken);
     }
 }
