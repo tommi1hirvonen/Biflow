8185a664108cf6bbdb8f2c2c9ac520755669b0fe
Tommi Hirvonen <tommih_@hotmail.com>
2024-04-05 11:35:00 +0300

    [Ui] [Schedules] job tag filtering

--- Biflow.Ui/UserState/SchedulesPageState.cs
+++ Biflow.Ui/UserState/SchedulesPageState.cs
@@ -4,6 +4,8 @@ public class SchedulesPageState
 {
     public HashSet<(Guid JobId, string JobName)> JobFilter { get; } = [];
 
+    public HashSet<TagProjection> TagFilter = [];
+
     public string ScheduleFilter { get; set; } = "";
 
     public StateFilter StateFilter { get; set; } = StateFilter.All;
