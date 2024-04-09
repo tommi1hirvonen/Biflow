91acf9b5b587737d0751d287cfb871db3da353f7
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-04-09 20:24:41 +0300

    [Ui] management of schedule tags

---
+++ Biflow.Ui.Core/Mediator/Commands/Tag/DeleteUnusedScheduleTags.cs
@@ -0,0 +1,20 @@
+﻿namespace Biflow.Ui.Core;
+
+public record DeleteUnusedScheduleTagsCommand : IRequest<DeleteUnusedScheduleTagsResponse>;
+
+public record DeleteUnusedScheduleTagsResponse(IEnumerable<ScheduleTag> DeletedTags);
+
+internal class DeleteUnusedScheduleTagsCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
+    : IRequestHandler<DeleteUnusedScheduleTagsCommand, DeleteUnusedScheduleTagsResponse>
+{
+    public async Task<DeleteUnusedScheduleTagsResponse> Handle(DeleteUnusedScheduleTagsCommand request, CancellationToken cancellationToken)
+    {
+        using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        var tags = await context.ScheduleTags
+            .Where(t => !t.Schedules.Any())
+            .ToArrayAsync(cancellationToken);
+        context.ScheduleTags.RemoveRange(tags);
+        await context.SaveChangesAsync(cancellationToken);
+        return new(tags);
+    }
+}
