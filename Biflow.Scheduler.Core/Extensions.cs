81c0f40f837f8e9ea47893f09135ff5f796b4661
Tommi Hirvonen <tommih_@hotmail.com>
2024-04-24 13:19:01 +0300

    [Scheduler.Core] [Extensions] increased Quartz threadpool count

--- Biflow.Scheduler.Core/Extensions.cs
+++ Biflow.Scheduler.Core/Extensions.cs
@@ -9,7 +9,7 @@ public static class Extensions
     public static void AddSchedulerServices<TExecutionJob>(this IServiceCollection services)
         where TExecutionJob : ExecutionJobBase
     {
-        services.AddQuartz();
+        services.AddQuartz(options => options.UseDefaultThreadPool(maxConcurrency: 100));
         services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
         services.AddDbContextFactory<SchedulerDbContext>();
         services.AddExecutionBuilderFactory<SchedulerDbContext>();
