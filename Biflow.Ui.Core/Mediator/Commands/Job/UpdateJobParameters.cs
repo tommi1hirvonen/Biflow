8789565be40ddf3d14548328fc9aba2a72f4c424
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-05-08 18:13:35 +0300

    [Ui.Core] [UpdateJobParameters] fix deleting job params when inheriting capturing/inheriting/assigning steps exists

--- Biflow.Ui.Core/Mediator/Commands/Job/UpdateJobParameters.cs
+++ Biflow.Ui.Core/Mediator/Commands/Job/UpdateJobParameters.cs
@@ -9,7 +9,9 @@ internal class UpdateJobParametersCommandHandler(IDbContextFactory<AppDbContext>
     {
         using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
         var job = await context.Jobs
-            .Include(j => j.JobParameters)
+            .Include(j => j.JobParameters).ThenInclude(p => p.InheritingStepParameters)
+            .Include(j => j.JobParameters).ThenInclude(p => p.CapturingSteps)
+            .Include(j => j.JobParameters).ThenInclude(p => p.AssigningStepParameters)
             .FirstOrDefaultAsync(j => j.JobId == request.Job.JobId, cancellationToken);
         if (job is null)
         {
