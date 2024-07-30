8b1ff3cd7c10974d3ae9024bc9557ba75536761a
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-07-30 15:56:22 +0300

    [Ui.Core] [CreateUser] fix bug when adding new users with data tables or jobs

--- Biflow.Ui.Core/Mediator/Commands/User/CreateUser.cs
+++ Biflow.Ui.Core/Mediator/Commands/User/CreateUser.cs
@@ -12,7 +12,34 @@ internal class CreateUserCommandHandler(
     public async Task Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
         using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        
+        var jobIds = request.User.Jobs
+            .Select(j => j.JobId)
+            .ToArray();
+        var dataTableIds = request.User.DataTables
+            .Select(dt => dt.DataTableId)
+            .ToArray();
+
+        var jobs = await context.Jobs
+            .Where(j => jobIds.Contains(j.JobId))
+            .ToArrayAsync(cancellationToken);
+        var dataTables = await context.MasterDataTables
+            .Where(t => dataTableIds.Contains(t.DataTableId))
+            .ToArrayAsync(cancellationToken);
+
+        request.User.Jobs.Clear();
+        request.User.DataTables.Clear();
+
+        foreach (var job in jobs)
+        {
+            request.User.Jobs.Add(job);
+        }
+        foreach (var dataTable in dataTables)
+        {
+            request.User.DataTables.Add(dataTable);
+        }
         context.Users.Add(request.User);
+
         if (authenticationResolver.AuthenticationMethod == AuthenticationMethod.BuiltIn)
         {
             var hash = BC.HashPassword(request.PasswordModel.Password);
