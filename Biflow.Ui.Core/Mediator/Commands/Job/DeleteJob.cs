5efef00aafbdd7fc598be3451674a2d0812b7515
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-09-17 08:27:14 +0300

    [Ui.Core] simplified DeleteJob, DeleteStep, DeleteSchedule navigation property includes cascade deletes take care of deleting these navigation properties

--- Biflow.Ui.Core/Mediator/Commands/Job/DeleteJob.cs
+++ Biflow.Ui.Core/Mediator/Commands/Job/DeleteJob.cs
@@ -10,21 +10,20 @@ internal class DeleteJobCommandHandler(
     public async Task Handle(DeleteJobCommand request, CancellationToken cancellationToken)
     {
         using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        
+        // Manually controlling transactions is allowed since AppDbContext does not use retry-on-failure execution strategy.
         using var transaction = context.Database.BeginTransaction();
+        
         var jobToRemove = await context.Jobs
             .Include(j => j.JobParameters)
             .ThenInclude(j => j.AssigningStepParameters)
             .ThenInclude(p => p.Step)
             .Include(j => j.Steps)
-            .ThenInclude(s => s.StepSubscriptions)
             .Include(j => j.Steps)
             .ThenInclude(s => s.Dependencies)
             .Include(j => j.Steps)
             .ThenInclude(s => s.Depending)
             .Include($"{nameof(Job.Steps)}.{nameof(IHasStepParameters.StepParameters)}")
-            .Include(j => j.JobSubscriptions)
-            .Include(j => j.JobTagSubscriptions)
-            .Include(j => j.Schedules)
             .Include(j => j.JobSteps)
             .FirstOrDefaultAsync(j => j.JobId == request.JobId, cancellationToken);
         if (jobToRemove is not null)
