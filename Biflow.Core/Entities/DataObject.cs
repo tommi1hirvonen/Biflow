a4d219191403d227dc83d26574842f7ec639f5f1
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-09-25 18:58:47 +0300

    [Core] [DataObject] replaced partial method with partial property

--- Biflow.Core/Entities/DataObject.cs
+++ Biflow.Core/Entities/DataObject.cs
@@ -81,32 +81,32 @@ public partial class DataObject : IDataObject
 
     public static string CreateTableUri(string server, string database, string schema, string table)
     {
-        server = NonAsciiCharsRegex().Replace(server, Replacement);
-        database = NonAsciiCharsRegex().Replace(database, Replacement);
-        schema = NonAsciiCharsRegex().Replace(schema, Replacement);
-        table = NonAsciiCharsRegex().Replace(table, Replacement);
+        server = NonAsciiCharsRegex.Replace(server, Replacement);
+        database = NonAsciiCharsRegex.Replace(database, Replacement);
+        schema = NonAsciiCharsRegex.Replace(schema, Replacement);
+        table = NonAsciiCharsRegex.Replace(table, Replacement);
         return $"table://{server}/{database}/{schema}/{table}";
     }
 
     public static string CreateDatasetUri(string workspaceName, string datasetName)
     {
-        workspaceName = NonAsciiCharsRegex().Replace(workspaceName, Replacement);
-        datasetName = NonAsciiCharsRegex().Replace(datasetName, Replacement);
+        workspaceName = NonAsciiCharsRegex.Replace(workspaceName, Replacement);
+        datasetName = NonAsciiCharsRegex.Replace(datasetName, Replacement);
         return $"pbi://{workspaceName}/{datasetName}";
     }
 
     public static string CreateTabularUri(string server, string model, string? table, string? partition)
     {
-        model = NonAsciiCharsRegex().Replace(model, Replacement);
+        model = NonAsciiCharsRegex.Replace(model, Replacement);
         if (partition is not null && table is not null)
         {
-            table = NonAsciiCharsRegex().Replace(table, Replacement);
-            partition = NonAsciiCharsRegex().Replace(partition, Replacement);
+            table = NonAsciiCharsRegex.Replace(table, Replacement);
+            partition = NonAsciiCharsRegex.Replace(partition, Replacement);
             return $"tabular://{server}/{model}/{table}/{partition}";
         }
         else if (table is not null)
         {
-            table = NonAsciiCharsRegex().Replace(table, Replacement);
+            table = NonAsciiCharsRegex.Replace(table, Replacement);
             return $"tabular://{server}/{model}/{table}";
         }
 
@@ -115,13 +115,13 @@ public partial class DataObject : IDataObject
 
     public static string CreateQlikUri(string appName)
     {
-        appName = NonAsciiCharsRegex().Replace(appName, Replacement);
+        appName = NonAsciiCharsRegex.Replace(appName, Replacement);
         return $"qlik://{appName}";
     }
 
     public static string CreateBlobUri(string accountName, string containerName, string blobPath)
     {
-        blobPath = NonAsciiCharsRegex().Replace(blobPath, Replacement);
+        blobPath = NonAsciiCharsRegex.Replace(blobPath, Replacement);
         return $"blob://{accountName}/{containerName}/{blobPath}";
     }
 
@@ -134,5 +134,5 @@ public partial class DataObject : IDataObject
     private const string Replacement = "_";
 
     [GeneratedRegex(@"[^\u0000-\u007F]+", RegexOptions.IgnoreCase, "en-US")]
-    private static partial Regex NonAsciiCharsRegex();
+    private static partial Regex NonAsciiCharsRegex { get; }
 }
