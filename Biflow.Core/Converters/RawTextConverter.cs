0740f9f251e2a36d9f8fb4c2ae9c77568b2844cc
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-10-11 21:18:06 +0300

    [Core] [QlikAutomationRun] include error as raw text

---
+++ Biflow.Core/Converters/RawTextConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace Biflow.Core.Converters;
+
+public class RawTextConverter : JsonConverter<string?>
+{
+    public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return JsonNode.Parse(ref reader)?.ToString();
+    }
+
+    public override void Write(Utf8JsonWriter writer, string? value, JsonSerializerOptions options)
+    {
+        if (value is null)
+        {
+            writer.WriteNullValue();
+            return;
+        }
+        JsonNode.Parse(value)?.WriteTo(writer);
+    }
+}
--- Biflow.Core/Entities/QlikCloudEnvironment/QlikAutomationRun.cs
+++ Biflow.Core/Entities/QlikCloudEnvironment/QlikAutomationRun.cs
@@ -1,3 +1,9 @@
-﻿namespace Biflow.Core.Entities;
+﻿using Biflow.Core.Converters;
+using System.Text.Json.Serialization;
 
-public record QlikAutomationRun(string Id, QlikAutomationRunStatus Status);
+namespace Biflow.Core.Entities;
+
+public record QlikAutomationRun(
+    string Id,
+    QlikAutomationRunStatus Status,
+    [property:JsonConverter(typeof(RawTextConverter))] string? Error);
