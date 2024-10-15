aa11e69032499c17b7f27b4e21c5723006adb90c
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-10-15 21:23:35 +0300

    [Core.Test] [QlikCloudClientDeserializeTests] fix test

--- Biflow.Core.Test/Deserialize/QlikCloudClientDeserializeTests.cs
+++ Biflow.Core.Test/Deserialize/QlikCloudClientDeserializeTests.cs
@@ -8,7 +8,7 @@ public class QlikCloudClientDeserializeTests
     private static readonly QlikCloudEnvironment client = CreateClient();
 
     [Fact]
-    public void QlikCloudClientId_NotEmptyGuid() => Assert.NotEqual(client.QlikCloudEnvironmentId, Guid.Empty);
+    public void QlikCloudEnvironmentId_NotEmptyGuid() => Assert.NotEqual(client.QlikCloudEnvironmentId, Guid.Empty);
 
     [Fact]
     public void ApiToken_Empty() => Assert.Empty(client.ApiToken);
@@ -21,7 +21,7 @@ public class QlikCloudClientDeserializeTests
             EnvironmentUrl = "my_env_url",
             ApiToken = "api_token"
         };
-        client.SetPrivatePropertyValue("QlikCloudClientId", Guid.NewGuid());
+        client.SetPrivatePropertyValue("QlikCloudEnvironmentId", Guid.NewGuid());
         return client.JsonRoundtrip(EnvironmentSnapshot.JsonSerializerOptionsPreserveReferences);
     }
 }
