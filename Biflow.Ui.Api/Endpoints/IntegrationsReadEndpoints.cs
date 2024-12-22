65eef160fe96870b266ee021a793bdc1fd58a5b0
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-12-22 14:08:57 +0200

    [Ui.Api] IntegrationsReadEndpoints with SQL connections

---
+++ Biflow.Ui.Api/Endpoints/IntegrationsReadEndpoints.cs
@@ -0,0 +1,40 @@
+using Biflow.Core.Constants;
+using Biflow.Ui.Core;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Api.Endpoints;
+
+[UsedImplicitly]
+public abstract class IntegrationsReadEndpoints : IEndpoints
+{
+    public static void MapEndpoints(WebApplication app)
+    {
+        var apiKeyEndpointFilterFactory = app.Services.GetRequiredService<ApiKeyEndpointFilterFactory>();
+        var endpointFilter = apiKeyEndpointFilterFactory.Create([Scopes.IntegrationsRead]);
+        
+        var group = app.MapGroup("/integrations")
+            .WithTags(Scopes.IntegrationsRead)
+            .AddEndpointFilter(endpointFilter);
+
+        group.MapGet("/sqlconnections", async (ServiceDbContext dbContext, CancellationToken cancellationToken) => 
+                await dbContext.SqlConnections
+                    .AsNoTracking()
+                    .OrderBy(c => c.ConnectionId)
+                    .ToArrayAsync(cancellationToken)
+            )
+            .WithDescription("Get all SQL connections. Sensitive connection strings will be replaced with an empty value.")
+            .WithName("GetSqlConnections");
+        
+        group.MapGet("/sqlconnections/{sqlConnectionId:guid}",
+            async (ServiceDbContext dbContext, Guid sqlConnectionId, CancellationToken cancellationToken) =>
+            {
+                var connection = await dbContext.SqlConnections
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(c => c.ConnectionId == sqlConnectionId, cancellationToken);
+                return connection is null ? Results.NotFound() : Results.Ok(connection);
+            })
+            .WithDescription("Get SQL connection by id. Sensitive connection string will be replaced with an empty value.")
+            .WithName("GetSqlConnection");
+    }
+}
