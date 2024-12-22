e20ee8a7e1ca46493b73cfb4b62921369db57d29
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-12-22 16:54:11 +0200

    [Ui.Api] SubscriptionsReadEndpoints

---
+++ Biflow.Ui.Api/Endpoints/SubscriptionsReadEndpoints.cs
@@ -0,0 +1,55 @@
+using Biflow.Core.Constants;
+using Biflow.Core.Entities;
+using Biflow.Ui.Core;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biflow.Ui.Api.Endpoints;
+
+[UsedImplicitly]
+public abstract class SubscriptionsReadEndpoints : IEndpoints
+{
+    public static void MapEndpoints(WebApplication app)
+    {
+        var apiKeyEndpointFilterFactory = app.Services.GetRequiredService<ApiKeyEndpointFilterFactory>();
+        var endpointFilter =
+            apiKeyEndpointFilterFactory.Create([Scopes.SubscriptionsRead, Scopes.JobsRead, Scopes.UsersRead]);
+        
+        var group = app.MapGroup("/subscriptions")
+            .WithTags($"{Scopes.SubscriptionsRead}, {Scopes.JobsRead}, {Scopes.UsersRead}")
+            .AddEndpointFilter(endpointFilter);
+        
+        group.MapGet("", async (ServiceDbContext dbContext, CancellationToken cancellationToken) =>
+                await dbContext.Subscriptions
+                    .AsNoTracking()
+                    .Include(s => s.User)
+                    .Include(s => ((JobSubscription)s).Job)
+                    .Include(s => ((JobTagSubscription)s).Job)
+                    .Include(s => ((JobTagSubscription)s).Tag)
+                    .Include(s => ((StepSubscription)s).Step)
+                    .Include(s => ((TagSubscription)s).Tag)
+                    .OrderBy(s => s.UserId)
+                    .ThenBy(s => s.SubscriptionType)
+                    .ToArrayAsync(cancellationToken)
+            )
+            .WithDescription("Get all subscriptions")
+            .WithName("GetSubscriptions");
+        
+        group.MapGet("/{subscriptionId:guid}",
+            async (ServiceDbContext dbContext, Guid subscriptionId, CancellationToken cancellationToken) =>
+            {
+                var subscription = await dbContext.Subscriptions
+                    .AsNoTracking()
+                    .Include(s => s.User)
+                    .Include(s => ((JobSubscription)s).Job)
+                    .Include(s => ((JobTagSubscription)s).Job)
+                    .Include(s => ((JobTagSubscription)s).Tag)
+                    .Include(s => ((StepSubscription)s).Step)
+                    .Include(s => ((TagSubscription)s).Tag)
+                    .FirstOrDefaultAsync(s => s.SubscriptionId == subscriptionId, cancellationToken);
+                return subscription is null ? Results.NotFound() : Results.Ok(subscription);
+            })
+            .WithDescription("Get subscription by id")
+            .WithName("GetSubscription");
+    }
+}
