29c655e41872c17444c879d36d772120bf1b9348
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-12-22 10:17:23 +0200

    [Ui.Api] [JobsReadEndpoints] indentation changes

--- Biflow.Ui.Api/Endpoints/JobsReadEndpoints.cs
+++ Biflow.Ui.Api/Endpoints/JobsReadEndpoints.cs
@@ -32,33 +32,35 @@ public abstract class JobsReadEndpoints : IEndpoints
             .WithName("GetJobs");
 
         group.MapGet("/{jobId:guid}",
-            async (ServiceDbContext dbContext, Guid jobId, CancellationToken cancellationToken) =>
-        {
-            var job = await dbContext.Jobs
-                .Include(j => j.JobParameters)
-                .Include(j => j.JobConcurrencies)
-                .Include(j => j.Tags)
-                .Include(j => j.Steps)
-                .Include(j => j.Schedules)
-                .FirstOrDefaultAsync(j => j.JobId == jobId, cancellationToken);
-            return job is null ? Results.NotFound() : Results.Ok(job);
-        }).WithDescription("Get job by id. Collection properties of steps and schedules are not loaded and will be empty.")
+            async (ServiceDbContext dbContext, Guid jobId, CancellationToken cancellationToken) => 
+            {
+                var job = await dbContext.Jobs
+                    .Include(j => j.JobParameters)
+                    .Include(j => j.JobConcurrencies)
+                    .Include(j => j.Tags)
+                    .Include(j => j.Steps)
+                    .Include(j => j.Schedules)
+                    .FirstOrDefaultAsync(j => j.JobId == jobId, cancellationToken);
+                return job is null ? Results.NotFound() : Results.Ok(job);
+            })
+            .WithDescription("Get job by id. Collection properties of steps and schedules are not loaded and will be empty.")
             .WithName("GetJob");
         
         group.MapGet("/steps/{stepId:guid}",
             async (ServiceDbContext dbContext, Guid stepId, CancellationToken cancellationToken) =>
-        {
-            var step = await dbContext.Steps
-                .Include($"{nameof(IHasStepParameters.StepParameters)}.{nameof(StepParameterBase.ExpressionParameters)}")
-                .Include($"{nameof(IHasStepParameters.StepParameters)}.{nameof(StepParameterBase.InheritFromJobParameter)}")
-                .Include(s => (s as JobStep)!.TagFilters.OrderBy(t => t.TagId))
-                .Include(s => s.Dependencies.OrderBy(d => d.DependantOnStepId))
-                .Include(s => s.DataObjects.OrderBy(d => d.ObjectId))
-                .Include(s => s.Tags.OrderBy(t => t.TagId))
-                .Include(s => s.ExecutionConditionParameters.OrderBy(p => p.ParameterId))
-                .FirstOrDefaultAsync(s => s.StepId == stepId, cancellationToken);
-            return step is null ? Results.NotFound() : Results.Ok(step);
-        }).WithDescription("Get step by id")
+            {
+                var step = await dbContext.Steps
+                    .Include($"{nameof(IHasStepParameters.StepParameters)}.{nameof(StepParameterBase.ExpressionParameters)}")
+                    .Include($"{nameof(IHasStepParameters.StepParameters)}.{nameof(StepParameterBase.InheritFromJobParameter)}")
+                    .Include(s => (s as JobStep)!.TagFilters.OrderBy(t => t.TagId))
+                    .Include(s => s.Dependencies.OrderBy(d => d.DependantOnStepId))
+                    .Include(s => s.DataObjects.OrderBy(d => d.ObjectId))
+                    .Include(s => s.Tags.OrderBy(t => t.TagId))
+                    .Include(s => s.ExecutionConditionParameters.OrderBy(p => p.ParameterId))
+                    .FirstOrDefaultAsync(s => s.StepId == stepId, cancellationToken);
+                return step is null ? Results.NotFound() : Results.Ok(step);
+            })
+            .WithDescription("Get step by id")
             .WithName("GetStep");
     }
 }
