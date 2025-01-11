33b6de6a453b409ff41207713ec714cc34ad2a9b
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2025-01-11 13:25:10 +0200

    [Ui.Core] [NotFoundException] ctor with multi-property key

--- Biflow.Ui.Core/Exceptions/NotFoundException.cs
+++ Biflow.Ui.Core/Exceptions/NotFoundException.cs
@@ -9,6 +9,13 @@ public class NotFoundException<T> : NotFoundException
     public NotFoundException(int id) : base($"Object of type {typeof(T).Name} with id {id} was not found.")
     {
     }
+
+    public NotFoundException(params (string Field, Guid Value)[] key)
+        : base($"Object of type {typeof(T).Name} with key {{ " +
+               string.Join(", ", key.Select(x => $"{x.Field}={x.Value}")) +
+               " } was not found.")
+    {
+    }
 }
 
 public class NotFoundException(string message) : Exception(message);
