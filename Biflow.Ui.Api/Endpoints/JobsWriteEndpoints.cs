e133e1f727cfc5e892fcd785b2aae2b9709c7ec5
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2025-01-17 20:19:18 +0200

    [Ui.Api] [JobsWriteEndpoints] rearrange endpoints

--- Biflow.Ui.Api/Endpoints/JobsWriteEndpoints.cs
+++ Biflow.Ui.Api/Endpoints/JobsWriteEndpoints.cs
@@ -38,23 +38,6 @@ public abstract class JobsWriteEndpoints : IEndpoints
             .WithDescription("Create a new job")
             .WithName("CreateJob");
         
-        group.MapPost("/tags",
-            async ([FromBody] TagDto tagDto, IMediator mediator, LinkGenerator linker, HttpContext ctx, CancellationToken cancellationToken) =>
-            {
-                var command = new CreateJobTagCommand(
-                    TagName: tagDto.TagName,
-                    Color: tagDto.Color,
-                    SortOrder: tagDto.SortOrder);
-                var tag = await mediator.SendAsync(command, cancellationToken);
-                var url = linker.GetUriByName(ctx, "GetJobTag", new { tagId = tag.TagId });
-                return Results.Created(url, tag);
-            })
-            .ProducesProblem(StatusCodes.Status409Conflict)
-            .ProducesValidationProblem()
-            .Produces<JobTag>(StatusCodes.Status201Created)
-            .WithDescription("Create a new job tag")
-            .WithName("CreateJobTag");
-        
         group.MapPut("/{jobId:guid}",
             async (Guid jobId, [FromBody] JobDto request, IMediator mediator, CancellationToken cancellationToken) =>
             {
@@ -77,6 +60,17 @@ public abstract class JobsWriteEndpoints : IEndpoints
             .Produces<Job>()
             .WithDescription("Update an existing job")
             .WithName("UpdateJob");
+        
+        group.MapDelete("/{jobId:guid}", async (Guid jobId, IMediator mediator, CancellationToken cancellationToken) =>
+            {
+                var command = new DeleteJobCommand(jobId);
+                await mediator.SendAsync(command, cancellationToken);
+                return Results.NoContent();
+            })
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status204NoContent)
+            .WithDescription("Delete a job")
+            .WithName("DeleteJob");
 
         group.MapPut("/{jobId:guid}/concurrencies",
             async ([FromRoute] Guid jobId, [FromBody] JobConcurrencyDto[] concurrencies,
@@ -92,6 +86,47 @@ public abstract class JobsWriteEndpoints : IEndpoints
             .Produces(StatusCodes.Status204NoContent)
             .WithDescription("Update job concurrencies for an existing job")
             .WithName("UpdateJobConcurrencies");
+        
+        group.MapPost("/{jobId:guid}/tags/{tagId:guid}",
+            async (Guid jobId, Guid tagId, IMediator mediator, CancellationToken cancellationToken) =>
+            {
+                var command = new CreateJobTagRelationCommand(jobId, tagId);
+                await mediator.SendAsync(command, cancellationToken);
+                return Results.NoContent();
+            })
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status204NoContent)
+            .WithDescription("Create a job tag relation")
+            .WithName("CreateJobTagRelation");
+        
+        group.MapDelete("/{jobId:guid}/tags/{tagId:guid}",
+            async (Guid jobId, Guid tagId, IMediator mediator, CancellationToken cancellationToken) =>
+            {
+                var command = new DeleteJobTagRelationCommand(jobId, tagId);
+                await mediator.SendAsync(command, cancellationToken);
+                return Results.NoContent();
+            })
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status204NoContent)
+            .WithDescription("Delete a job tag relation")
+            .WithName("DeleteJobTagRelation");
+        
+        group.MapPost("/tags",
+            async ([FromBody] TagDto tagDto, IMediator mediator, LinkGenerator linker, HttpContext ctx, CancellationToken cancellationToken) =>
+            {
+                var command = new CreateJobTagCommand(
+                    TagName: tagDto.TagName,
+                    Color: tagDto.Color,
+                    SortOrder: tagDto.SortOrder);
+                var tag = await mediator.SendAsync(command, cancellationToken);
+                var url = linker.GetUriByName(ctx, "GetJobTag", new { tagId = tag.TagId });
+                return Results.Created(url, tag);
+            })
+            .ProducesProblem(StatusCodes.Status409Conflict)
+            .ProducesValidationProblem()
+            .Produces<JobTag>(StatusCodes.Status201Created)
+            .WithDescription("Create a new job tag")
+            .WithName("CreateJobTag");
 
         group.MapPut("/tags/{tagId:guid}",
             async (Guid tagId, [FromBody] TagDto tagDto, IMediator mediator, CancellationToken cancellationToken) =>
@@ -109,41 +144,6 @@ public abstract class JobsWriteEndpoints : IEndpoints
             .Produces<JobTag>()
             .WithDescription("Update an existing job tag")
             .WithName("UpdateJobTag");
-        
-        group.MapPost("/{jobId:guid}/tags/{tagId:guid}",
-            async (Guid jobId, Guid tagId, IMediator mediator, CancellationToken cancellationToken) =>
-            {
-                var command = new CreateJobTagRelationCommand(jobId, tagId);
-                await mediator.SendAsync(command, cancellationToken);
-                return Results.NoContent();
-            })
-            .ProducesProblem(StatusCodes.Status404NotFound)
-            .Produces(StatusCodes.Status204NoContent)
-            .WithDescription("Create a job tag relation")
-            .WithName("CreateJobTagRelation");
-        
-        group.MapDelete("/{jobId:guid}", async (Guid jobId, IMediator mediator, CancellationToken cancellationToken) =>
-            {
-                var command = new DeleteJobCommand(jobId);
-                await mediator.SendAsync(command, cancellationToken);
-                return Results.NoContent();
-            })
-            .ProducesProblem(StatusCodes.Status404NotFound)
-            .Produces(StatusCodes.Status204NoContent)
-            .WithDescription("Delete a job")
-            .WithName("DeleteJob");
-        
-        group.MapDelete("/{jobId:guid}/tags/{tagId:guid}",
-            async (Guid jobId, Guid tagId, IMediator mediator, CancellationToken cancellationToken) =>
-            {
-                var command = new DeleteJobTagRelationCommand(jobId, tagId);
-                await mediator.SendAsync(command, cancellationToken);
-                return Results.NoContent();
-            })
-            .ProducesProblem(StatusCodes.Status404NotFound)
-            .Produces(StatusCodes.Status204NoContent)
-            .WithDescription("Delete a job tag relation")
-            .WithName("DeleteJobTagRelation");
         
         group.MapDelete("/tags/{tagId:guid}", async (Guid tagId, IMediator mediator, CancellationToken cancellationToken) =>
             {
