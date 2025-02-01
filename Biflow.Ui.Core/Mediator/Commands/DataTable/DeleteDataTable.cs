0fbe29be1bcce65fbbc6947d9fcc61c7a2ced022
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2025-02-01 14:55:42 +0200

    [Ui.Core] move DeleteDataTable from UI project

--- Biflow.Ui.Core/Mediator/Commands/DataTable/DeleteDataTable.cs
+++ Biflow.Ui.Core/Mediator/Commands/DataTable/DeleteDataTable.cs
@@ -1,7 +1,8 @@
-﻿namespace Biflow.Ui;
+﻿namespace Biflow.Ui.Core;
 
 public record DeleteDataTableCommand(Guid DataTableId) : IRequest;
 
+[UsedImplicitly]
 internal class DeleteDataTableCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
     : IRequestHandler<DeleteDataTableCommand>
 {
@@ -12,11 +13,9 @@ internal class DeleteDataTableCommandHandler(IDbContextFactory<AppDbContext> dbC
             .IgnoreQueryFilters()
             .Include(t => t.Lookups)
             .Include(t => t.DependentLookups)
-            .FirstOrDefaultAsync(t => t.DataTableId == request.DataTableId, cancellationToken);
-        if (table is not null)
-        {
-            context.MasterDataTables.Remove(table);
-            await context.SaveChangesAsync(cancellationToken);
-        }
+            .FirstOrDefaultAsync(t => t.DataTableId == request.DataTableId, cancellationToken)
+            ?? throw new NotFoundException<MasterDataTable>(request.DataTableId);
+        context.MasterDataTables.Remove(table);
+        await context.SaveChangesAsync(cancellationToken);
     }
 }
