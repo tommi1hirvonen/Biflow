b6ea57a6aef179b89edbd2ea6c4030bededaab92
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2025-02-14 20:51:05 +0200

    [Ui, Ui.Core] UpdateUserPasswordAdmin takes user id instead of username

--- Biflow.Ui.Core/Mediator/Commands/User/UpdateUserPasswordAdmin.cs
+++ Biflow.Ui.Core/Mediator/Commands/User/UpdateUserPasswordAdmin.cs
@@ -1,14 +1,15 @@
 ﻿using BC = BCrypt.Net.BCrypt;
 
-namespace Biflow.Ui;
+namespace Biflow.Ui.Core;
 
 /// <summary>
 /// Update the password for an existing user. Should only be used when the authentication mode is BuiltIn.
 /// </summary>
-/// <param name="Username">Username for the account</param>
+/// <param name="UserId">User id of the user</param>
 /// <param name="Password">New password</param>
-public record UpdateUserPasswordAdminCommand(string Username, string Password) : IRequest;
+public record UpdateUserPasswordAdminCommand(Guid UserId, string Password) : IRequest;
 
+[UsedImplicitly]
 internal class UpdateUserPasswordAdminCommandHandler(IDbContextFactory<AppDbContext> dbContextFactory)
     : IRequestHandler<UpdateUserPasswordAdminCommand>
 {
@@ -17,12 +18,12 @@ internal class UpdateUserPasswordAdminCommandHandler(IDbContextFactory<AppDbCont
         await using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
         var newHash = BC.HashPassword(request.Password);
         var affectedRows = await context.Users
-            .Where(u => u.Username == request.Username)
+            .Where(u => u.UserId == request.UserId)
             .ExecuteUpdateAsync(updates => updates
                 .SetProperty(u => EF.Property<string?>(u, "PasswordHash"), newHash), cancellationToken);
         if (affectedRows == 0)
         {
-            throw new ArgumentException($"No user found with username {request.Username}");
+            throw new NotFoundException<User>(request.UserId);
         }
     }
 }
