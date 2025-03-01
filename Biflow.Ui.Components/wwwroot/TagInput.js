e1ca849a47b7a11a31ba7b10b58a288ad805fb9f
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2025-03-01 21:13:51 +0200

    [Ui.Components] [GenericTagInput] improved focus/blur behaviour The tag input dropdown is now hidden when navigating away from the tag input with tab.

--- Biflow.Ui.Components/wwwroot/TagInput.js
+++ Biflow.Ui.Components/wwwroot/TagInput.js
@@ -1,37 +1,24 @@
-
-const listener = function onClickListener(event) {
-    const elem = event.target;
-    const autosuggests = document.querySelectorAll('.taginput-wrapper');
-    if (!elem.closest('.taginput-wrapper')) {
-        [].forEach.call(autosuggests, function (autosuggest) {
-            autosuggest.dotNetObject.invokeMethodAsync('TagInput_Hide');
-        });
-    } else {
-        [].forEach.call(autosuggests, function (autosuggest) {
-            if (elem.closest('.taginput-wrapper') != autosuggest) {
-                autosuggest.dotNetObject.invokeMethodAsync('TagInput_Hide');
-            }
-        });
+const blurListener = function onBlurListener(event) {
+    const wrapperElement = event.target.closest('.taginput-wrapper');
+    if (wrapperElement && wrapperElement.contains(event.relatedTarget) ) {
+        return;
     }
+    event.target.dotNetObject.invokeMethodAsync('TagInput_Hide');
 };
 
-export function calculateShowUp(listWrapper) {
-    listWrapper.classList.add('show');
+export function calculateShowUp(dropdownElement) {
+    dropdownElement.classList.add('show');
     const _vh = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-    const space = _vh - listWrapper.getBoundingClientRect().bottom;
-    if (space < 0) {
-        return true;
-    } else {
-        return false;
-    }
+    const space = _vh - dropdownElement.getBoundingClientRect().bottom;
+    return space < 0;
 }
 
-export function create(element, dotNetObject) {
-    element.dotNetObject = dotNetObject;
-    document.addEventListener('click', listener);
+export function create(inputElement, dotNetObject) {
+    inputElement.dotNetObject = dotNetObject;
+    inputElement.addEventListener('blur', blurListener);
 }
 
-export function dispose(element) {
-    element.dotNetObject = null;
-    document.removeEventListener('click', listener);
+export function dispose(inputElement) {
+    inputElement.dotNetObject = null;
+    inputElement.removeEventListener('blur', blurListener);
 }
