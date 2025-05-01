bbdc0d02be623ddbf645a0a5ef91ab7c505a4ff6
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2025-05-01 15:06:21 +0300

    Add unit tests for ExeTaskStatusResponse

---
+++ Biflow.Proxy.Core.Test/ExeTaskStatusResponseTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using Xunit;
+
+namespace Biflow.Proxy.Core.Test;
+
+public class ExeTaskStatusResponseTests
+{
+    private static readonly JsonSerializerOptions Options = new() { WriteIndented = true };
+    
+    [Fact]
+    public void TaskRunningStatusJsonExpected()
+    {
+        const string expected = """
+                                {
+                                  "$type": "Running"
+                                }
+                                """;
+        ExeTaskStatusResponse status = new ExeTaskRunningStatusResponse();
+        var json = JsonSerializer.Serialize(status, Options);
+        Assert.Equal(expected, json);
+    }
+    
+    [Fact]
+    public void TaskFailedStatusJsonExpected()
+    {
+        const string expected = """
+                                {
+                                  "$type": "Failed",
+                                  "ErrorMessage": "Test"
+                                }
+                                """;
+        ExeTaskStatusResponse status = new ExeTaskFailedStatusResponse { ErrorMessage = "Test" };
+        var json = JsonSerializer.Serialize(status, Options);
+        Assert.Equal(expected, json);
+    }
+    
+    [Fact]
+    public void TaskSucceededStatusJsonExpected()
+    {
+        const string expected = """
+                                {
+                                  "$type": "Succeeded",
+                                  "Response": {
+                                    "ExitCode": 0,
+                                    "Output": "Test",
+                                    "OutputIsTruncated": false,
+                                    "ErrorOutput": "Test",
+                                    "ErrorOutputIsTruncated": false,
+                                    "InternalError": null
+                                  }
+                                }
+                                """;
+        ExeTaskStatusResponse status = new ExeTaskSucceededStatusResponse
+        {
+            Result = new ExeProxyRunResult
+            {
+                ExitCode = 0,
+                Output = "Test",
+                OutputIsTruncated = false,
+                ErrorOutput = "Test",
+                ErrorOutputIsTruncated = false,
+                InternalError = null
+            }
+        };
+        var json = JsonSerializer.Serialize(status, Options);
+        Assert.Equal(expected, json);
+    }
+}
