896cd1e062350f9f02841d78eec0a0e5902935e0
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2025-05-01 23:49:12 +0300

    [Aspire.AppHost] added proxy API project

--- Biflow.Aspire.AppHost/Program.cs
+++ Biflow.Aspire.AppHost/Program.cs
@@ -1,11 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Biflow_Proxy_WebApp>("proxyapi");
+var proxyApi = builder.AddProject<Projects.Biflow_Proxy_WebApp>("proxyapi");
+proxyApi.WithUrl($"{proxyApi.GetEndpoint("http")}/swagger", "Swagger");
 
 var executorApi = builder.AddProject<Projects.Biflow_Executor_WebApp>("executorapi");
+executorApi.WithUrl($"{executorApi.GetEndpoint("http")}/swagger", "Swagger");
 
 var schedulerApi = builder.AddProject<Projects.Biflow_Scheduler_WebApp>("schedulerapi")
     .WithReference(executorApi);
+schedulerApi.WithUrl($"{schedulerApi.GetEndpoint("http")}/swagger", "Swagger");
 
 builder.AddProject<Projects.Biflow_Ui>("frontend")
     .WithReference(schedulerApi)
