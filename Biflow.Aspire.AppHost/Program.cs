79ab4027ca5684238e6d256c69065ea8afabb468
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2025-05-24 14:49:02 +0300

    [Aspire.AppHost] add connection string for app database

--- Biflow.Aspire.AppHost/Program.cs
+++ Biflow.Aspire.AppHost/Program.cs
@@ -3,19 +3,25 @@ var builder = DistributedApplication.CreateBuilder(args);
 var proxyApi = builder.AddProject<Projects.Biflow_ExecutorProxy_WebApp>("proxyapi");
 proxyApi.WithUrl($"{proxyApi.GetEndpoint("http")}/swagger", "Swagger");
 
-var executorApi = builder.AddProject<Projects.Biflow_Executor_WebApp>("executorapi");
+var appDbContext = builder.AddConnectionString("AppDbContext");
+
+var executorApi = builder.AddProject<Projects.Biflow_Executor_WebApp>("executorapi")
+    .WithReference(appDbContext);
 executorApi.WithUrl($"{executorApi.GetEndpoint("http")}/swagger", "Swagger");
 
 var schedulerApi = builder.AddProject<Projects.Biflow_Scheduler_WebApp>("schedulerapi")
+    .WithReference(appDbContext)
     .WithReference(executorApi);
 schedulerApi.WithUrl($"{schedulerApi.GetEndpoint("http")}/swagger", "Swagger");
 
 builder.AddProject<Projects.Biflow_Ui>("frontend")
+    .WithReference(appDbContext)
     .WithReference(schedulerApi)
     .WithReference(executorApi)
     .WithExternalHttpEndpoints();
 
 builder.AddProject<Projects.Biflow_Ui_Api>("api")
+    .WithReference(appDbContext)
     .WithReference(schedulerApi)
     .WithReference(executorApi)
     .WithExternalHttpEndpoints();
