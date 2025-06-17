5aed6d20de7c6b12dbce58da76938fa5ce2d1230
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2025-06-17 16:33:54 +0300

    [Executor.Core] [ExecutionOptions] changed PollingIntervalMs to optional by giving it a default value

--- src/Biflow.Executor.Core/ExecutionOptions.cs
+++ src/Biflow.Executor.Core/ExecutionOptions.cs
@@ -5,5 +5,5 @@ namespace Biflow.Executor.Core;
 internal class ExecutionOptions
 {
     [Range(500, 30000)]
-    public int PollingIntervalMs { get; [UsedImplicitly] init; }
+    public int PollingIntervalMs { get; [UsedImplicitly] init; } = 5000; // Default to 5000 milliseconds
 }
