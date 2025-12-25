9d38ca46713a12298b4b22101c7cdfc70ca01edb
Tommi Hirvonen <tommih_@hotmail.com>
2025-12-25 14:13:10 +0200

    Aspire launches APIs with noBrowser profile

--- src/Biflow.Aspire.AppHost/Program.cs
+++ src/Biflow.Aspire.AppHost/Program.cs
@@ -1,30 +1,35 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var proxyApi = builder.AddProject<Projects.Biflow_ExecutorProxy_WebApp>("proxyapi")
+var proxyApi = builder
+    .AddProject<Projects.Biflow_ExecutorProxy_WebApp>("proxyapi", options => options.LaunchProfileName = "noBrowser")
     .WithHttpHealthCheck("/health");
 proxyApi.WithUrl($"{proxyApi.GetEndpoint("http")}/swagger", "Swagger");
 
 var appDbContext = builder.AddConnectionString("AppDbContext");
 
-var executorApi = builder.AddProject<Projects.Biflow_Executor_WebApp>("executorapi")
+var executorApi = builder
+    .AddProject<Projects.Biflow_Executor_WebApp>("executorapi", options => options.LaunchProfileName = "noBrowser")
     .WithHttpHealthCheck("/health")
     .WithReference(appDbContext);
 executorApi.WithUrl($"{executorApi.GetEndpoint("http")}/swagger", "Swagger");
 
-var schedulerApi = builder.AddProject<Projects.Biflow_Scheduler_WebApp>("schedulerapi")
+var schedulerApi = builder
+    .AddProject<Projects.Biflow_Scheduler_WebApp>("schedulerapi")
     .WithHttpHealthCheck("/health")
     .WithReference(appDbContext)
     .WithReference(executorApi);
 schedulerApi.WithUrl($"{schedulerApi.GetEndpoint("http")}/swagger", "Swagger");
 
-builder.AddProject<Projects.Biflow_Ui>("frontend")
+builder
+    .AddProject<Projects.Biflow_Ui>("frontend")
     .WithHttpHealthCheck("/health")
     .WithReference(appDbContext)
     .WithReference(schedulerApi)
     .WithReference(executorApi)
     .WithExternalHttpEndpoints();
 
-builder.AddProject<Projects.Biflow_Ui_Api>("api")
+builder
+    .AddProject<Projects.Biflow_Ui_Api>("api", options => options.LaunchProfileName = "noBrowser")
     .WithHttpHealthCheck("/health")
     .WithReference(appDbContext)
     .WithReference(schedulerApi)
--- src/Biflow.Aspire.SqlAppHost/Program.cs
+++ src/Biflow.Aspire.SqlAppHost/Program.cs
@@ -1,19 +1,23 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
-var proxyApi = builder.AddProject<Projects.Biflow_ExecutorProxy_WebApp>("proxyapi")
+var proxyApi = builder
+    .AddProject<Projects.Biflow_ExecutorProxy_WebApp>("proxyapi", options => options.LaunchProfileName = "noBrowser")
     .WithHttpHealthCheck("/health");
 proxyApi.WithUrl($"{proxyApi.GetEndpoint("http")}/swagger", "Swagger");
 
-var sql = builder.AddSqlServer("sql")
+var sql = builder
+    .AddSqlServer("sql")
     .WithLifetime(ContainerLifetime.Persistent);
 
 var db = sql.AddDatabase("AppDbContext");
 
-var migrations = builder.AddProject<Projects.Biflow_Aspire_MigrationService>("migrations")
+var migrations = builder
+    .AddProject<Projects.Biflow_Aspire_MigrationService>("migrations")
     .WithReference(db)
     .WaitFor(db);
 
-var executorApi = builder.AddProject<Projects.Biflow_Executor_WebApp>("executorapi")
+var executorApi = builder
+    .AddProject<Projects.Biflow_Executor_WebApp>("executorapi", options => options.LaunchProfileName = "noBrowser")
     .WithHttpHealthCheck("/health")
     .WithReference(db)
     .WithReference(migrations)
@@ -21,7 +25,8 @@ var executorApi = builder.AddProject<Projects.Biflow_Executor_WebApp>("executora
     .WaitForCompletion(migrations);
 executorApi.WithUrl($"{executorApi.GetEndpoint("http")}/swagger", "Swagger");
 
-var schedulerApi = builder.AddProject<Projects.Biflow_Scheduler_WebApp>("schedulerapi")
+var schedulerApi = builder
+    .AddProject<Projects.Biflow_Scheduler_WebApp>("schedulerapi", options => options.LaunchProfileName = "noBrowser")
     .WithHttpHealthCheck("/health")
     .WithReference(db)
     .WithReference(migrations)
@@ -30,7 +35,8 @@ var schedulerApi = builder.AddProject<Projects.Biflow_Scheduler_WebApp>("schedul
     .WaitForCompletion(migrations);
 schedulerApi.WithUrl($"{schedulerApi.GetEndpoint("http")}/swagger", "Swagger");
 
-builder.AddProject<Projects.Biflow_Ui>("frontend")
+builder
+    .AddProject<Projects.Biflow_Ui>("frontend")
     .WithHttpHealthCheck("/health")
     .WithReference(db)
     .WithReference(migrations)
@@ -40,7 +46,8 @@ builder.AddProject<Projects.Biflow_Ui>("frontend")
     .WaitFor(db)
     .WaitForCompletion(migrations);
 
-builder.AddProject<Projects.Biflow_Ui_Api>("api")
+builder
+    .AddProject<Projects.Biflow_Ui_Api>("api", options => options.LaunchProfileName = "noBrowser")
     .WithHttpHealthCheck("/health")
     .WithReference(db)
     .WithReference(migrations)
